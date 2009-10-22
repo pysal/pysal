d8e69d776e42d11983f5c0d172e5e95f2b9b2452
Charles Schimdt <schmidtc@gmail.com>
2009-10-22 22:38:30 +0000

    Adding setup.py and README.

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+setup.py for PySAL package.
+"""
+
+from distutils.core import setup, Extension
+
+setup(name = 'pysal',
+      description = 'PySAL: Python Spatial Analysis Library',
+      author = 'Luc Anselin, Serge Rey, Charles Schmidt, Andrew Winslow',
+      url = 'http://pysal.org/',
+      version = '0.1',
+      packages = ['pysal', 
+                  'pysal.cg',
+                  'pysal.core', 
+                  'pysal.core._FileIO', 
+                  'pysal.core._FileIO.pyshp', 
+                  'pysal.esda', 
+                  'pysal.weights'],
+      ext_modules = [Extension('pysal.core._FileIO.pyshp._shp', ['pysal/core/_FileIO/pyshp/_shp.c'])],
+      package_data = {'pysal':['examples/shp_test/*','examples/*.*','examples/README']},
+      requires = ['scipy']
+     )
+
