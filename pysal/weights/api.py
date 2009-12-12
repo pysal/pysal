24b7ee3d23904df2a3d6328f0384d5a230ee2182
Serge Rey <sjsrey@gmail.com>
2009-12-12 17:19:14 +0000

    - more set operations

--- pysal/weights/api.py
+++ pysal/weights/api.py
@@ -7,7 +7,19 @@ w_queen,observation_ids=weights.queen_from_shapefile('file.shp', id_variable_nam
 
 w_regime=weights.regime(regime_variable, observation_ids)
 
+
+
+# set operations
+# members of same regime and contiguous non-regime units
 w_hybrid=w_regime.union(w_queen)
+
+# members of same regime that are contiguous
+w_regime_contiguities = w_regime.intersection(w_queen)
+
+# contiguous but not in same regime = border connections
+set_ids=set(observation_ids)
+w_border = w_queen.intersection(set_ids.difference(w_regime))
+
 
 # subsetting example
 w_us_county_rook=weights.rook_from_shapefile("uscounties.shp")
@@ -20,13 +32,9 @@ w_california_rook=w_us_county.subset(california_ids)
 # aggregation
 w_us_states_rook=w_us_county.aggregate(state_ids, observation_ids)
 
-
 w_lattice = weights.lat2gal(10,10)
 
-
-
-w_idist=weights.inverse_distance(data,p=2)
-# inverse distance will
+w_idist=weights.inverse_distance(data,p=2,row_standardization=True)
 
 w_nn=weights.nearest_neighbors(data,k=2,p=2)
 
@@ -36,11 +44,11 @@ w_shared=weights.shared_boundary(shapefile)
 # have to think about exterior polygons
 
 
-
-
 # lag api
-
+# observation_ids are necessary to ensure alignment of elements of y with
+# weights
 w_lattice.lag(y,observation_ids)
+
 
 numpy_array_w,observation_ids = w_lattice.full(observation_ids=None)
 numpy_array_w,observation_ids = w_lattice.full(observation_ids=observation_ids)
@@ -55,8 +63,6 @@ gwt = pysal.open('my_lattice.gwt','w')
 gwt.write(w_lattice)
 gwt.close()
 
-
 # reading
-
 w_lattice=pysal.open('my_lattice.gal','r').read()
 
