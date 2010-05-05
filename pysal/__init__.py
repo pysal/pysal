37defe2faad83607fade5b27f0b8ac4618785db3
Serge Rey <sjsrey@gmail.com>
2010-05-05 17:57:07 +0000

    - reorganizing (temporary breakage will be fixed)

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -47,9 +47,9 @@ from esda.mapclassify import User_Defined,Max_P
 from esda.moran import Moran, Moran_BV, Moran_BV_matrix, Moran_Local
 from econometrics import Jarque_Bera,Ols
 from inequality.theil import Theil,TheilD,TheilDSim
-from spatial_dyanmics.markov.markov import Markov
-from spatial_dyanmics.markov.ergodic import steady_state
-from spatial_dyanmics.mobility.rank import Theta,SpatialTau
+from spatial_dyanmics.markov import Markov
+from spatial_dyanmics.ergodic import steady_state
+from spatial_dyanmics.rank import Theta,SpatialTau
 from region.maxp import Maxp,Maxp_LISA
 from weights import W,lat2W,regime_weights,comb,full,shimbel,order,higher_order
 from weights.Distance import knnW, Kernel, DistanceBand
