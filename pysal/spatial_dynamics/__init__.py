5c654e889aabf7abd554a14975332c11ad4e85a6
Xinyue Ye <xinyueye@gmail.com>
2010-06-14 06:20:23 +0000

    start to add modules

--- pysal/spatial_dynamics/__init__.py
+++ pysal/spatial_dynamics/__init__.py
@@ -7,6 +7,9 @@
 from markov import *
 from rank import *
 from ergodic import *
+from knox import *
+from lisatp import *
+from lisamarkov import *
 
 __all__ = filter(lambda s:not s.startswith("_"),dir())
 
