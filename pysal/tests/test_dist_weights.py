605fc41691d595a168a6245779bd7fcf45cd689e
Serge Rey <sjsrey@gmail.com>
2010-07-19 23:32:37 +0000

    - distance weights test suite

--- pysal/tests/test_dist_weights.py
+++ pysal/tests/test_dist_weights.py
@@ -1,14 +1,93 @@
 import os
 import unittest
 import pysal
+import numpy as np
 
 class _TestDistanceWeights(unittest.TestCase):
     def setUp(self):
         """ Setup the rtree and binning contiguity weights"""
-        self.polyShp = '../examples/virginia.shp'
-        self.pointShp = '../examples/juvenile.shp'
-    def test_A(self):
-        self.assertEqual(1,1)
+        self.polyShp = '../examples/columbus.shp'
+        self.points=[(10, 10), (20, 10), (40, 10), (15, 20), (30, 20), (30, 30)]
+
+    def test_knnW(self):
+        x=np.indices((5,5))
+        x,y=np.indices((5,5))
+        x.shape=(25,1)
+        y.shape=(25,1)
+        data=np.hstack([x,y])
+        wnn2=pysal.knnW(data,k=2)
+        wnn4=pysal.knnW(data,k=4)
+        wnn4.neighbors[0]
+        self.assertEqual(wnn4.neighbors[0], [1, 5, 6, 2])
+        self.assertEqual(wnn2.neighbors[5], [0, 6])
+        self.assertEqual(wnn2.pct_nonzero, 0.080000000000000002)
+        wnn3e=pysal.knnW(data,p=2,k=3)
+        self.assertEqual(wnn3e.neighbors[0],[1, 5, 6])
+        wc=pysal.knnW_from_shapefile(self.polyShp)
+        self.assertEqual(wc.pct_nonzero, 0.040816326530612242)
+        self.assertEqual(wc.neighbors[0], [ 2, 1])
+        wc3=pysal.knnW_from_shapefile(self.polyShp,k=3,idVariable="POLYID")
+        self.assertEqual(wc3.weights[1],[1, 1, 1])
+        self.assertEqual(wc3.neighbors[1], [ 3, 2, 4])
+
+    def test_kernel(self):
+        kw=pysal.Kernel(self.points)
+        self.assertEqual(kw.weights[0], [1.0, 0.50000004999999503, 0.44098306152674649])
+        kw15=pysal.Kernel(self.points,bandwidth=15.0)
+        self.assertEqual(kw15[0], {0: 1.0, 1: 0.33333333333333337, 3: 0.2546440075000701})
+        self.assertEqual(kw15.bandwidth[0],15.)
+        self.assertEqual(kw15.bandwidth[-1],15.)
+        bw=[25.0,15.0,25.0,16.0,14.5,25.0]
+        kwa=pysal.Kernel(self.points,bandwidth=bw)
+        self.assertEqual(kwa.weights[0], [1.0, 0.59999999999999998,
+                                          0.55278640450004202,
+                                          0.10557280900008403])
+        self.assertEqual(kwa.neighbors[0], [0, 1, 3, 4])
+        self.assertEqual(kwa.bandwidth[0], 25.)
+        self.assertEqual(kwa.bandwidth[1], 15.)
+        self.assertEqual(kwa.bandwidth[2], 25.)
+        self.assertEqual(kwa.bandwidth[3], 16.)
+        self.assertEqual(kwa.bandwidth[4], 14.5)
+        self.assertEqual(kwa.bandwidth[5], 25.)
+        kwea=pysal.Kernel(self.points,fixed=False)
+        self.assertEqual(kwea.weights[0], [1.0, 0.10557289844279438,
+                                           9.9999990066379496e-08])
+        l=kwea.bandwidth.tolist()
+        self.assertEqual(l,[[11.180341005532938], [11.180341005532938],
+                           [20.000002000000002], [11.180341005532938],
+                           [14.142137037944515], [18.027758180095585]])
+        kweag=pysal.Kernel(self.points,fixed=False,function='gaussian')
+        self.assertEqual(kweag.weights[0], [0.3989422804014327,
+                                            0.26741902915776961,
+                                            0.24197074871621341])
+        l=kweag.bandwidth.tolist()
+        self.assertEqual(l,[[11.180341005532938], [11.180341005532938],
+                            [20.000002000000002], [11.180341005532938],
+                            [14.142137037944515], [18.027758180095585]])
+
+        kw = pysal.kernelW_from_shapefile(self.polyShp,idVariable='POLYID')
+        self.assertEqual(kw.weights[1], [0.20524787824004365,
+                                         0.0070787731484506233, 1.0,
+                                         0.23051223027663015])
+        kwa=pysal.adaptive_kernelW_from_shapefile(self.polyShp)
+        self.assertEqual(kwa.weights[0], [9.9999990066379496e-08, 1.0,
+                                          0.031789067677363891])
+
+    def test_threshold(self):
+        md = pysal.min_threshold_dist_from_shapefile(self.polyShp)
+        self.assertEqual(md, 0.61886415807685413)
+        wid=pysal.threshold_continuousW_from_array(self.points,11.2)
+        self.assertEqual(wid.weights[0], [0.10000000000000001,
+                                          0.089442719099991588])
+        wid2=pysal.threshold_continuousW_from_array(self.points,11.2,alpha=-2.0)
+        self.assertEqual(wid2.weights[0], [0.01, 0.0079999999999999984])
+        w=pysal.threshold_continuousW_from_shapefile(self.polyShp,0.62,idVariable="POLYID")
+        self.assertEqual(w.weights[1], [1.6702346893743276,
+                                        1.7250729841938044])
+
+
+      
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(_TestDistanceWeights)
 
