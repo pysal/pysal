1c48dd7444108c5257e5c7f77fd2302a564bec10
Phil Stephens <phil.stphns@gmail.com>
2010-07-28 06:54:51 +0000

    Specified GeoDa Center as <ORGANIZATION> in the LICENSE file. Added elements to the setup script. Edited the Wiki.

--- setup.py
+++ setup.py
@@ -9,10 +9,15 @@ from pysal.version import version
 
 setup(name = 'pysal',
       description = 'PySAL: Python Spatial Analysis Library',
+      long_description = open('INSTALL.txt'),
       author = 'Luc Anselin, Serge Rey, Charles Schmidt, Andrew Winslow',
       author_email = 'srey@asu.edu',
+      maintainer = 'Phil Stephens',
+      maintainer_email = 'phil.spam@asu.edu',
       url = 'http://pysal.org/',
+      download_url = 'http://code.google.com/p/pysal/downloads/list',
       version = version,
+      license = 'BSD License',
       packages = ['pysal', 
                   'pysal.cg',
                   'pysal.core', 
@@ -23,6 +28,21 @@ setup(name = 'pysal',
                   'pysal.region',
                   'pysal.tests',
                   'pysal.weights'],
+
       package_data = {'pysal':['examples/shp_test/*','examples/*.*','examples/README']},
-      requires = ['scipy']
+
+      requires = ['scipy'],
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Education',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: GIS',
+          ]
+
      )
