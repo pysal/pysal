d9b8e4caf399df8eb477b8772955cb64b0e347c0
Phil Stephens <phil.stphns@gmail.com>
2010-07-28 05:43:36 +0000

    Mac OS X installer done, requires pypi package 'bdist_mpkg'. This shell script now checks for platform-specific builds. Uncomment line 3 to make a fresh PDF of the docs for inclusion in these builds.

--- pkg.sh
+++ pkg.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-cd doc/
-make latex
-cd build/latex
-make all-pdf
-cd ../../../
-python setup.py sdist --formats=gztar,zip
-python setup.py bdist_wininst
+#uncomment next to build fresh PDF of docs
+#cd doc/;make latex;cd build/latex;make all-pdf;cd ../../../
+
+#here we build the distros
+
+platform=$(uname)
+if [[ $platform == 'Darwin' ]]; then
+    python setup.py sdist --formats=gztar,zip
+    python setup.py bdist --formats=wininst
+    bdist_mpkg setup.py build
+elif [[ $platform == 'win32' ]]; then
+    python setup.py bdist --formats=wininst,msi
+elif [[ $platform == 'Linux' ]]; then
+    python setup.py bdist --formats=rpm
+fi
 rm -rf build/
