8b6b7f649754ad5ce6611c250f9d4005bfede533
Serge Rey <sjsrey@gmail.com>
2010-07-30 19:09:41 +0000

    - pre build changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -107,7 +107,7 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "%s v%s Reference Guide (DRAFT)" % (project, version)
+html_title = "%s v%s Reference Guide" % (project, version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
