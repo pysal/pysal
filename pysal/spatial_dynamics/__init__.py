937d9cb68b87e62fb0966277bef069e44dcac3f2
Serge Rey <sjsrey@gmail.com>
2011-01-05 22:17:07 +0000

    - delete lambda magic

--- pysal/spatial_dynamics/__init__.py
+++ pysal/spatial_dynamics/__init__.py
@@ -8,10 +8,3 @@ from markov import *
 from rank import *
 from ergodic import *
 from directional import *
-#xinyue has to add knox from knox import * 
-#ditto from lisatp import *
-#ditto from lisamarkov import *
-
-__all__ = filter(lambda s:not s.startswith("_"),dir())
-
-
