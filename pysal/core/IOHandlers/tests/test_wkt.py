5de8c6e6f1216972e10441c5a5a51884408115d8
Phil Stephens <phil.stphns@gmail.com>
2011-01-25 00:03:35 +0000

    new test_wkt.py for wkt reader module.

---
+++ pysal/core/IOHandlers/tests/test_wkt.py
@@ -0,0 +1,24 @@
+import unittest
+
+class test_WKTReader(unittest.TestCase):
+    def test___init__(self):
+        # w_kt_reader = WKTReader(*args, **kwargs)
+        assert False # TODO: implement your test here
+
+    def test_close(self):
+        # w_kt_reader = WKTReader(*args, **kwargs)
+        # self.assertEqual(expected, w_kt_reader.close())
+        assert False # TODO: implement your test here
+
+    def test_open(self):
+        # w_kt_reader = WKTReader(*args, **kwargs)
+        # self.assertEqual(expected, w_kt_reader.open())
+        assert False # TODO: implement your test here
+
+    def test_seek(self):
+        # w_kt_reader = WKTReader(*args, **kwargs)
+        # self.assertEqual(expected, w_kt_reader.seek(n))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
