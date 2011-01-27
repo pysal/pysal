6e6dd85e37c45ae28f100eb377c63f32aa954377
Dani Arribas <dreamessence@gmail.com>
2011-01-27 23:05:30 +0000

    Adding imports for spatial diagnostics into doctests so it is replicable externally

--- pysal/spreg/diagnostics_sp.py
+++ pysal/spreg/diagnostics_sp.py
@@ -97,7 +97,7 @@ class LMtests:
     presence of a spatial error model (simple and robust version) and joint presence
     of both a spatial lag as well as a spatial error model. 
 
-    >>> lms = LMtests(ols, w)
+    >>> lms = pysal.spreg.diagnostics_sp.LMtests(ols, w)
 
     LM error test (rounded to 6 decimals)
 
@@ -201,7 +201,7 @@ class MoranRes:
     expectation and variance to account for the fact it comes from residuals
     instead of an independent variable
 
-    >>> m = MoranRes(ols, w, z=True)
+    >>> m = pysal.spreg.diagnostics_sp.MoranRes(ols, w, z=True)
 
     Value of the Moran's I statistic rounded to 6 decimals
 
