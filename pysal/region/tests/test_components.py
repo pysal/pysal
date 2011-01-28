080879920c471aec8ada0af636e563f8e78ed130
David Folch <dfolch@gmail.com>
2011-01-28 15:11:58 +0000

    adding unit tests for check contiguity

---
+++ pysal/region/tests/test_components.py
@@ -0,0 +1,28 @@
+
+import unittest
+import pysal
+
+
+class _Test_Components(unittest.TestCase):
+    def setUp(self):
+        self.w = pysal.lat2W(5, 5)
+
+    def test_check_contiguity(self):
+        result = pysal.region.check_contiguity(self.w,[0,1,2,3,4],4)
+        self.assertEquals(result, True)
+        result = pysal.region.check_contiguity(self.w,[0,1,2,3,4],3)
+        self.assertEquals(result, False)
+        result = pysal.region.check_contiguity(self.w,[0,1,2,3,4],0)
+        self.assertEquals(result, True)
+        result = pysal.region.check_contiguity(self.w,[0,1,2,3,4],1)
+        self.assertEquals(result, False)
+        
+
+suite = unittest.TestLoader().loadTestsFromTestCase(_Test_Components)
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+
+
