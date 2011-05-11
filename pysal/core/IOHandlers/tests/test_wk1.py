a3f0acf7572a2aaf9535fd195cb6e10154beeb84
Myhungha Hwang <mhwang4@gmail.com>
2011-05-11 17:56:16 +0000

    adding an IO handler for wk1 file

---
+++ pysal/core/IOHandlers/tests/test_wk1.py
@@ -0,0 +1,42 @@
+import unittest
+import pysal
+from pysal.core.IOHandlers.wk1 import Wk1IO
+import tempfile
+import os
+
+class test_Wk1IO(unittest.TestCase):
+    def setUp(self):
+        self.test_file = test_file = '../../../examples/spat-sym-us.wk1'
+        self.obj = Wk1IO(test_file, 'r')
+
+    def test_close(self):
+        f = self.obj
+        f.close()
+        self.failUnlessRaises(ValueError, f.read)
+
+    def test_read(self):
+        w = self.obj.read()
+        self.assertEqual(46, w.n)
+        self.assertEqual(4.0869565217391308, w.mean_neighbors)
+        self.assertEqual([1.0, 1.0, 1.0, 1.0], w[1].values())
+
+    def test_seek(self):
+        self.test_read()
+        self.failUnlessRaises(StopIteration, self.obj.read)
+        self.obj.seek(0)
+        self.test_read()
+
+    def test_write(self):
+        w = self.obj.read()
+        f = tempfile.NamedTemporaryFile(suffix='.wk1',dir="../../../examples")
+        fname = f.name
+        f.close()
+        o = pysal.open(fname,'w')
+        o.write(w)
+        o.close()
+        wnew =  pysal.open(fname,'r').read()
+        self.assertEqual( wnew.pct_nonzero, w.pct_nonzero)
+        os.remove(fname)
+
+if __name__ == '__main__':
+    unittest.main()
