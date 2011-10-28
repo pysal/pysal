13bf322615d579403dea96df5684cd2725ca2177
Charles Schimdt <schmidtc@gmail.com>
2011-10-28 20:46:32 +0000

    Adding 1st contrib, a wxPython based Weights file viewer.

---
+++ pysal/contrib/weights_viewer/transforms.py
@@ -0,0 +1,208 @@
+import pysal
+
+__author__ = "Charles R Schmidt <Charles.R.Schmidt@asu.edu>"
+
+class WorldToViewTransform(object):
+    """
+    An abstract class modeling a View window.
+    Supports Panning, Zooming, Resizing.
+
+    Is observable.
+
+    Parameters:
+    worldExtent -- Extent,List -- Extent of the world, left,lower,right,upper in world coords
+    pixel_width -- int -- intial width of the view in pixels
+    pixel_height -- int -- intial height of the view in pixels
+
+    Notes:
+    World coordinates are expected to increase the X and Y direction.
+    Pixel coordinates are inverted in the Y direction.
+
+    This class helps tranform world coordinates to screen coordinates.
+    To transform a GraphicsMatrix,
+    matrix.Scale(1.0/model.scale,-1.0/model.scale)
+    matrix.Translate(*model.offset)
+    
+    The transforms will be applied in reverse order,
+    The coordinates will first be translated to the origin (of the current view).
+    The coordinates will then be scaled.
+
+    Eg.
+    >>> view = WorldToViewTransform([-180,-90,180,90],500,500)
+    """
+    def __init__(self,worldExtent,pixel_width,pixel_height):
+        """ Intialize the view to the extent of the world """
+        self.__pixel_width = float(pixel_width)
+        self.__pixel_height = float(pixel_height)
+        self.__world = worldExtent
+        self.extent = worldExtent
+        # In World Coords
+    def __copy__(self):
+        return WorldToViewTransform(self.extent,self.__pixel_width,self.__pixel_height)
+    copy = __copy__
+    def __get_offset(self):
+        """ 
+        Returns the offset of the top left corner of the current view in world coords.
+        Move the world this many units to aling it with the view.
+        """
+        return self.__offset
+    def __set_offset(self,value):
+        """
+        Set the Offset of the top left corner in world coords.
+        """
+        assert len(value) == 2
+        self.__offset = value
+    offset = property(fget=__get_offset,fset=__set_offset)
+    def __get_scale(self):
+        """ Returns the current scale in units/pixel """
+        return self.__scale
+    def __set_scale(self,value):
+        """ Sets the current scale in units/pixel """
+        self.__scale = value
+    scale = property(fget=__get_scale,fset=__set_scale)
+    def __get_extent(self):
+        """Returns the extent of the current view in World Coordinates."""
+        left,upper = self.pixel_to_world(0,0)
+        right,lower = self.pixel_to_world(self.__pixel_width,self.__pixel_height)
+        return pysal.cg.Rectangle(left,lower,right,upper)
+    def __set_extent(self,value):
+        """ Set the extent of the current view in World Coordinates.
+            Preserve fixed scale, take the max of (sx,sy).
+
+            Use this to zoom to a sepcific region when you know the region's 
+            bbox in world coords.
+        """
+        left,lower,right,upper = value
+        width = abs(right-left)
+        height = abs(upper-lower)
+        sx = width/self.__pixel_width
+        sy = height/self.__pixel_height
+        self.__scale = max(sx,sy)
+
+        #The offset translate the world to the origin.
+        #The X offset + world.left == 0
+        #The Y offset + world.upper == 0
+
+        # Move the offset a little, so that the center of the extent is in the center of the view.
+        oleft = (left+(width/2.0)) - (self.__pixel_width*self.__scale/2.0)
+        oupper = (upper-height/2.0) + (self.__pixel_height*self.__scale/2.0)
+
+        #self.__offset = (-left,-upper) # in world coords
+        self.__offset = (-oleft,-oupper) # in world coords
+    extent = property(fget=__get_extent,fset=__set_extent)
+    def __get_width(self):
+        """ Returns the width of the current view in world coords """
+        return self.__pixel_width*self.scale
+    def __set_width(self, value):
+        """
+        Sets the width of the current view, value in pixels
+        
+        Eg.
+        >>> view = WorldToViewTransform([0,0,100,100],500,500)
+        >>> view.extent[:]
+        [0.0, 0.0, 100.0, 100.0]
+        >>> view.width = 250
+        >>> view.extent[:]
+        [0.0, 0.0, 50.0, 100.0]
+        """
+        if self.__pixel_width != value:
+            self.__pixel_width = value
+    width = property(fget=__get_width,fset=__set_width)
+    def __get_height(self):
+        """ Returns the height of the current view in world coords """
+        return self.__pixel_height*self.scale
+    def __set_height(self, value):
+        """
+        Sets the height of the current view, value in pixels
+        
+        Eg.
+        >>> view = WorldToViewTransform([0,0,100,100],500,500)
+        >>> view.extent[:]
+        [0.0, 0.0, 100.0, 100.0]
+        >>> view.height = 250
+        >>> view.extent[:]
+        [0.0, 50.0, 100.0, 100.0]
+        """
+        if self.__pixel_height != value:
+            self.__pixel_height = value
+    height = property(fget=__get_height,fset=__set_height)
+    def __get_pixel_size(self):
+        """
+        Set and Return the current size of the view in pixels.
+        """
+        return self.__pixel_width,self.__pixel_height
+    def __set_pixel_size(self,value):
+        w,h = value
+        if self.__pixel_width != w:
+            self.__pixel_width = w
+        if self.__pixel_height != h:
+            self.__pixel_height = h
+    pixel_size = property(fget=__get_pixel_size,fset=__set_pixel_size)
+
+    def pan(self,dpx,dpy):
+        """ 
+        Pan the view by (dpx,dpy) pixel coordinates.
+        
+        Positive deltas move the world right and down.
+        Negative deltas move the world left and up.
+
+        Eg.
+        >>> view = WorldToViewTransform([0,0,100,100],500,500)
+        >>> view.pan(500,0)
+        >>> view.extent[:]
+        [-100.0, 0.0, 0.0, 100.0]
+        >>> view.pan(-500,500)
+        >>> view.extent[:]
+        [0.0, 100.0, 100.0, 200.0]
+        >>> view.pan(0,-500)
+        >>> view.extent[:]
+        [0.0, 0.0, 100.0, 100.0]
+        >>> view.pan(490,490)
+        >>> view.extent[:]
+        [-98.0, 98.0, 2.0, 198.0]
+        >>> view.pan(-490,-490)
+        >>> view.extent[:]
+        [0.0, 0.0, 100.0, 100.0]
+        """
+        ogx,ogy = self.__offset
+        s = self.scale
+        self.__offset = ogx+(dpx*s),ogy-(dpy*s)
+    def pan_to(self,extent):
+        initScale = self.scale
+        self.extent = extent
+        self.scale = initScale
+    def pixel_to_world(self,px,py):
+        """
+        Returns the world coordinates of the Pixel (px,py).
+
+        Eg.
+        >>> view = WorldToViewTransform([0,0,100,100],500,500)
+        >>> view.pixel_to_world(0,0)
+        (0.0, 100.0)
+        >>> view.pixel_to_world(500,500)
+        (100.0, 0.0)
+        """
+        sx = self.scale
+        sy = -sx
+        ogx,ogy = self.__offset
+        return px*sx - ogx, py*sy - ogy
+    def world_to_pixel(self,x,y):
+        """
+        Returns the pixel of the world coordinate (x,y).
+
+        Eg.
+        >>> view = WorldToViewTransform([0,0,100,100],500,500)
+        >>> view.world_to_pixel(0,0)
+        (0.0, 500.0)
+        >>> view.world_to_pixel(100,100)
+        (500.0, -0.0)
+        """
+        sx = self.scale
+        sy = -sx
+        ogx,ogy = self.__offset
+        return (x+ogx)/sx, (y+ogy)/sy
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
+    view = WorldToViewTransform([0,0,100,100],500,500)
---
+++ pysal/contrib/weights_viewer/weights_viewer.py
@@ -0,0 +1,162 @@
+import wx
+import pysal
+from transforms import WorldToViewTransform
+
+__author__ = "Charles R Schmidt <Charles.R.Schmidt@asu.edu>"
+
+POINT_RADIUS = 5
+BORDER_COLOR = wx.Colour(0,0,0,255)
+SELECTION_COLOR = wx.Colour(255,128,0,255)
+NEIGHBORS_COLOR = wx.Colour(128,255,0,255)
+
+class WeightsMap(wx.Panel):
+    """ Display a Weights Inspection Map """
+    def __init__(self, parent, geo, w_obj):
+        wx.Panel.__init__(self,parent,size=(600,600))
+        self.status = parent.CreateStatusBar(3)
+        self.status.SetStatusWidths([-1,-2,-2])
+        self.status.SetStatusText('No Selection',0)
+        self.Bind(wx.EVT_SIZE, self.onSize)
+        self.Bind(wx.EVT_PAINT, self.onPaint)
+        self.Bind(wx.EVT_MOUSE_EVENTS, self.onMouse)
+        self.trns = 0
+        self.background = (255,255,255,255)
+        w,h = self.GetSize()
+        self.buffer = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        self.geo = geo
+        if geo.type == pysal.cg.shapes.Polygon:
+            self.drawfunc = self.drawpoly
+            self.locator = pysal.cg.PolygonLocator(geo)
+        elif geo.type == pysal.cg.shapes.Point:
+            self.drawfunc = self.drawpt
+            self.locator = pysal.cg.PointLocator(geo)
+        else:
+            raise TypeError, "Unsupported Type: %r"%(geo.type)
+        self.w = w_obj
+        self.transform = WorldToViewTransform(geo.bbox,w,h)
+        self.selection = None
+    def onMouse(self, evt):
+        x,y = evt.X,evt.Y
+        X,Y = self.transform.pixel_to_world(x,y)
+        if self.geo.type == pysal.cg.shapes.Polygon:
+            rs = self.locator.contains_point((X,Y))
+            if rs:
+                selection = rs[0].id-1
+                self.set_selection(selection)
+            else:
+                self.set_selection(None)
+        else:
+            print self.locator.nearest((X,Y))
+    def onSize(self, evt):
+        w,h = self.GetSize()
+        self.buffer = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+        self.transform = WorldToViewTransform(self.geo.bbox,w,h)
+        self.draw()
+    def onPaint(self, evt):
+        pdc = wx.PaintDC(self)
+        pdc.Clear()
+        self.draw()
+    def draw(self):
+        dc = wx.MemoryDC()
+        dc.SelectObject(self.buffer)
+        dc.SetBackground(wx.Brush(wx.Colour(*self.background)))
+        dc.Clear()
+        dc.SelectObject(wx.NullBitmap)
+        self.draw_shps(self.buffer)
+        cdc = wx.ClientDC(self)
+        cdc.DrawBitmap(self.buffer,0,0)
+    def drawpoly(self,gc,matrix,fill=False,ids=None):
+        geo = self.geo
+        pth = gc.CreatePath()
+        if not ids:
+            ids = xrange(len(geo))
+        for i in ids:
+            poly = geo.get(i)
+            parts = poly.parts
+            if poly.holes[0]:
+                parts = parts+poly.holes
+            for part in parts:
+                x,y = part[0]
+                pth.MoveToPoint(x,y)
+                for x,y in part[1:]:
+                    pth.AddLineToPoint(x,y)
+                pth.CloseSubpath()
+        pth.Transform(matrix)
+        if fill:
+            gc.FillPath(pth)
+        gc.StrokePath(pth)
+        return gc
+    def drawpt(self,gc,matrix,fill=False,ids=None):
+        r = POINT_RADIUS
+        radius = r/2.0
+        geo = self.geo
+        for pt in geo:
+            x,y = matrix.TransformPoint(*pt)
+            gc.DrawEllipse(x-radius,y-radius,r,r)
+        return gc
+    def draw_shps(self, buff, fill_color=None, ids=None):
+        transform = self.transform
+        dc = wx.MemoryDC()
+        dc.SelectObject(buff)
+        gc = wx.GraphicsContext.Create(dc)
+        gc.SetPen( gc.CreatePen(wx.Pen(BORDER_COLOR,1)) )
+        if fill_color != None:
+            gc.SetBrush( gc.CreateBrush(wx.Brush(fill_color)) )
+            fill = True
+        else:
+            fill = False
+        matrix = gc.CreateMatrix()
+        matrix.Scale(1./transform.scale,1./-transform.scale)
+        matrix.Translate(*transform.offset)
+        self.drawfunc(gc,matrix,fill,ids)
+    def set_selection(self,sel):
+        if self.selection == sel:
+            return
+        self.selection = sel
+
+        cdc = wx.ClientDC(self)
+        cdc.Clear()
+        cdc.DrawBitmap(self.buffer,0,0)
+        if sel != None:
+            w,h = self.transform.pixel_size
+            buff = wx.EmptyBitmapRGBA(w,h,alpha=self.trns)
+            id = self.w.id_order[sel]
+            neighbors = map(self.w.id_order.index,self.w.neighbors[id])
+            self.draw_shps(buff, SELECTION_COLOR, [sel])
+            self.draw_shps(buff, NEIGHBORS_COLOR, neighbors)
+            print sel,":",neighbors
+            cdc.DrawBitmap(buff,0,0)
+            stat0 = "Selection:%s"%id
+            stat1 = "Neighbors:%s"%(','.join(map(str,self.w.neighbors[id])))
+            stat2 = "Weights:%s"%(','.join(map(str,self.w.weights[id])))
+            self.status.SetStatusText(stat0,0)
+            self.status.SetStatusText(stat1,1)
+            self.status.SetStatusText(stat2,2)
+        else:
+            self.status.SetStatusText('No Selection',0)
+            self.status.SetStatusText('',1)
+            self.status.SetStatusText('',2)
+
+class WeightsMapApp(wx.App):
+    def __init__(self, geo=None, w=None, redirect=False):
+        if issubclass(type(geo),basestring):
+            geo = pysal.open(geo,'r')
+        self.geo = geo
+        if issubclass(type(w),basestring):
+            w = pysal.open(w,'r').read()
+        self.w = w
+        wx.App.__init__(self, redirect)
+    def OnInit(self):
+        self.SetAppName("Weights Inspector")
+        self.frame = wx.Frame(None,size=(600,600))
+        self.SetTopWindow(self.frame)
+        wm = WeightsMap(self.frame,self.geo,self.w)
+        self.frame.Show()
+        return True
+
+
+if __name__=='__main__':
+    shp = '../../examples/sids2.shp'
+    w = '../../examples/sids2.gal'
+    app = WeightsMapApp(shp,w)
+    app.MainLoop()
