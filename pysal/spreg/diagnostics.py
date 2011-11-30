Adding setup.cfg which is a generic config file that can hold parameters for other future objects as needed. Fixing floating point precision tests in several modules. Values agreed on 32 and 64 bit Python versions. Using new pysal.examples.get_path() method for connecting doc and unit tests to our example data. Devs take note.

pysal/spreg/diagnostics.py
@@ -39,7 +39,7 @@ def f_stat(reg):
 
     Read the DBF associated with the Columbus data. 
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -116,7 +116,7 @@ def t_stat(reg, z_stat=False):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -190,7 +190,7 @@ def r2(reg):
 
     Read the DBF associated with the Columbus data.
     
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector.
 
@@ -257,7 +257,7 @@ def ar2(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -319,7 +319,7 @@ def se_betas(reg):
 
     Read the DBF associated with the Columbus data. 
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector.
 
@@ -382,7 +382,7 @@ def log_likelihood(reg):
 
     Read the DBF associated with the Columbus data. 
     
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -407,7 +407,7 @@ def log_likelihood(reg):
     Print the result. 
 
     >>> testresult
-    -187.37723881214913
+    -187.3772388121491
 
     """
     n = reg.n       # (scalar) number of observations
@@ -446,7 +446,7 @@ def akaike(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -471,7 +471,7 @@ def akaike(reg):
     Print the result. 
 
     >>> testresult
-    380.75447762429826
+    380.7544776242982
 
     """
     n = reg.n       # (scalar) number of observations
@@ -511,7 +511,7 @@ def schwarz(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -536,7 +536,7 @@ def schwarz(reg):
     Print the results. 
 
     >>> testresult
-    386.42993851863014
+    386.42993851863008
 
     """
     n = reg.n      # (scalar) number of observations
@@ -577,7 +577,7 @@ def condition_index(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -656,7 +656,7 @@ def jarque_bera(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"), "r")
 
     Create the dependent variable vector. 
 
@@ -758,8 +758,8 @@ def breusch_pagan(reg, z=None):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
-
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"), "r")
+ 
     Create the dependent variable vector. 
 
     >>> y = np.array(db.by_col("CRIME"))
@@ -904,7 +904,7 @@ def white(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -1060,7 +1060,7 @@ def koenker_bassett(reg, z=None):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -1202,7 +1202,7 @@ def vif(reg):
 
     Read the DBF associated with the Columbus data.
 
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
 
     Create the dependent variable vector. 
 
@@ -1292,7 +1292,7 @@ def constant_check(array):
     >>> import pysal
     >>> import diagnostics
     >>> from ols import BaseOLS as OLS
-    >>> db = pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
     >>> y = np.array(db.by_col("CRIME"))
     >>> y = np.reshape(y, (49,1))
     >>> X = []

pysal/spreg/ols.py
@@ -122,7 +122,7 @@ class BaseOLS(RegressionProps):
 
     >>> import numpy as np
     >>> import pysal
-    >>> db=pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
     >>> y = np.array(db.by_col("CRIME"))
     >>> y = np.reshape(y, (49,1))
     >>> X = []
@@ -291,7 +291,7 @@ class OLS(BaseOLS, USER.DiagnosticBuilder):
     requires data to be passed in as numpy arrays so the user can read their
     data in using any method.  
 
-    >>> db=pysal.open("pysal/examples/columbus.dbf","r")
+    >>> db = pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
     
     Extract the HOVAL column (home values) from the DBF file and make it the
     dependent variable for the regression. Note that PySAL requires this to be
@@ -396,7 +396,7 @@ class OLS(BaseOLS, USER.DiagnosticBuilder):
     residuals is 0.2037 with a standardized value of 2.5918 and a p-value of
     0.009547.
 
-    >>> w = pysal.weights.rook_from_shapefile("pysal/examples/columbus.shp")
+    >>> w = pysal.weights.rook_from_shapefile(pysal.examples.get_path("columbus.shp"))
     >>> ols = pysal.spreg.OLS(y, X, w, name_y='home value', name_x=['income','crime'], name_ds='columbus')
     >>> ols.betas
     array([[ 46.42818268],

pysal/spreg/tests/test_diagnostics.py
@@ -8,7 +8,7 @@ from pysal.spreg.ols import BaseOLS as OLS
 
 
 # create regression object used by all the tests below
-db = pysal.open("pysal/examples/columbus.dbf", "r")
+db = pysal.open(pysal.examples.get_path("columbus.dbf"), "r")
 y = np.array(db.by_col("CRIME"))
 y = np.reshape(y, (49,1))
 X = []

pysal/spreg/tests/test_ols.py
@@ -6,7 +6,7 @@ from pysal.spreg import ols as OLS
 class Test_OLS(unittest.TestCase):
     """ setUp is called before each test function execution """
     def setUp(self):
-        db=pysal.open("pysal/examples/columbus.dbf","r")
+        db=pysal.open(pysal.examples.get_path("columbus.dbf"),"r")
         y = np.array(db.by_col("CRIME"))
         y = np.reshape(y, (49,1))
         X = []
@@ -128,7 +128,7 @@ class Test_OLS(unittest.TestCase):
         betas = np.array([[ 1.28624161], [ 0.22045774]])
         np.testing.assert_array_almost_equal(ols.betas, betas, decimal=8)
         # test spatial diagnostics
-        w = pysal.open('pysal/examples/columbus.gal', 'r').read()
+        w = pysal.open(pysal.examples.get_path('columbus.gal'), 'r').read()
         w.transform = 'r'
         ols = OLS.OLS(self.y, self.X, w=w)
         self.assertAlmostEquals(ols.lm_error[0], 5.2062139238820784)