d280763430a7823bf0a472f284a953c3caaa6b8a
Serge Rey <sjsrey@gmail.com>
2012-01-23 20:33:06 +0000

    - starting ols unittests

--- pysal/spreg/tests/test_ols.py
+++ pysal/spreg/tests/test_ols.py
@@ -1,14 +1,36 @@
 import unittest
+import numpy as np
+import pysal
 
 class TestBaseOLS(unittest.TestCase):
-    def test___init__(self):
-        # base_ol_s = BaseOLS(y, x, constant, robust, gwk, sig2n_k)
-        assert False # TODO: implement your test here
+    def setUp(self):
+        db = pysal.open(pysal.examples.get_path('columbus.dbf'),'r')
+        y = np.array(db.by_col("HOVAL"))
+        self.y = np.reshape(y, (49,1))
+        X = []
+        X.append(db.by_col("INC"))
+        X.append(db.by_col("CRIME"))
+        self.X = np.array(X).T
 
-class TestOLS(unittest.TestCase):
-    def test___init__(self):
-        # o_l_s = OLS(y, x, w, robust, gwk, sig2n_k, nonspat_diag, spat_diag, moran, vm, name_y, name_x, name_w, name_gwk, name_ds)
-        assert False # TODO: implement your test here
+    def test_ols(self):
+        ols = pysal.spreg.ols.BaseOLS(self.y,self.X)
+        np.testing.assert_array_almost_equal(ols.betas, np.array([[
+            46.42818268], [  0.62898397], [ -0.48488854]]))
+        vm = np.array([[  1.74022453e+02,  -6.52060364e+00,  -2.15109867e+00],
+           [ -6.52060364e+00,   2.87200008e-01,   6.80956787e-02],
+           [ -2.15109867e+00,   6.80956787e-02,   3.33693910e-02]])
+        np.testing.assert_array_almost_equal(ols.vm, vm,6)
+
+    def test_OLS(self):
+        ols = pysal.spreg.ols.OLS(self.y,self.X)
+        np.testing.assert_array_almost_equal(ols.t_stat[2][0], \
+                -2.65440864272,7)
+        np.testing.assert_array_almost_equal(ols.t_stat[2][1], \
+                0.0108745049098,7)
+        np.testing.assert_array_almost_equal(ols.r2, \
+                0.34951437785126105 ,7)
+
+
 
 if __name__ == '__main__':
     unittest.main()
