9b3a4229efbb9b2f6c8d3fa84435586f0bb55604
Charles Schimdt <schmidtc@gmail.com>
2012-02-20 16:18:07 +0000

    removing invalid __all__ from esda's init. See #194

--- pysal/esda/__init__.py
+++ pysal/esda/__init__.py
@@ -3,17 +3,8 @@
 =================================================
 
 """
-__all__=[]
-
 import mapclassify
 import moran
 import smoothing
 import getisord
 import geary
-
-__all__+=mapclassify.__all__
-__all__+=moran.__all__
-__all__+=getisord.__all__
-__all__+=geary.__all__
-__all__+=smoothing.__all__
-
