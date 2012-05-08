65a5d555082c8db67a9026cdc200a75ba750d2b1
Phil Stephens <phil.stphns@gmail.com>
2012-05-08 05:14:04 +0000

    Cleaning up.

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -5,7 +5,8 @@ Python Spatial Analysis Library
 
 Documentation
 -------------
-PySAL documentation is available in two forms: python docstrings and a html webpage at http://pysal.org/
+PySAL documentation is available in two forms: python docstrings and an html \
+        webpage at http://pysal.org/
 
 Available sub-packages
 ----------------------
@@ -30,40 +31,48 @@ Utilities
 `fileio`_
     Tool for file input and output, supports many well known file formats
 """
-import cg
-import core
+import pysal.cg
+import pysal.core
 
-from version import version
+from pysal.version import version
 
 # toplevel imports to be explicit
-from esda.moran import Moran, Moran_BV, Moran_BV_matrix, Moran_Local
-from esda.geary import Geary
-from esda.join_counts import Join_Counts
-from esda.mapclassify import quantile,binC,bin,bin1d,Equal_Interval,Percentiles
-from esda.mapclassify import Box_Plot,Quantiles,Std_Mean,Maximum_Breaks
-from esda.mapclassify import Natural_Breaks, Fisher_Jenks, Jenks_Caspall
-from esda.mapclassify import Jenks_Caspall_Sampled,Jenks_Caspall_Forced
-from esda.mapclassify import User_Defined,Max_P_Classifier,gadf
-from esda.mapclassify import K_classifiers
-from inequality.theil import Theil,TheilD,TheilDSim
-from region.maxp import Maxp,Maxp_LISA
-from spatial_dynamics import Markov, Spatial_Markov, LISA_Markov, SpatialTau, Theta
-from spatial_dynamics import ergodic
-from spatial_dynamics import directional
-from weights import W,lat2W,regime_weights,comb,full,shimbel,order,higher_order,remap_ids
-from weights.Distance import knnW, Kernel, DistanceBand
-from weights.Contiguity import buildContiguity
-from weights.spatial_lag import lag_spatial
-from weights.Wsets import w_union, w_intersection, w_difference
-from weights.Wsets import w_symmetric_difference, w_subset
-from weights.user import queen_from_shapefile, rook_from_shapefile, knnW_from_array,knnW_from_shapefile, threshold_binaryW_from_array, threshold_binaryW_from_shapefile, threshold_continuousW_from_array, threshold_continuousW_from_shapefile, kernelW, kernelW_from_shapefile, adaptive_kernelW, adaptive_kernelW_from_shapefile, min_threshold_dist_from_shapefile,build_lattice_shapefile
-import spreg
-import examples
+from pysal.esda.moran import Moran, Moran_BV, Moran_BV_matrix, Moran_Local
+from pysal.esda.geary import Geary
+from pysal.esda.join_counts import Join_Counts
+from pysal.esda.mapclassify import quantile, binC, bin, bin1d, Equal_Interval, \
+        Percentiles
+from pysal.esda.mapclassify import Box_Plot, Quantiles, Std_Mean, Maximum_Breaks
+from pysal.esda.mapclassify import Natural_Breaks, Fisher_Jenks, Jenks_Caspall
+from pysal.esda.mapclassify import Jenks_Caspall_Sampled, Jenks_Caspall_Forced
+from pysal.esda.mapclassify import User_Defined, Max_P_Classifier, gadf
+from pysal.esda.mapclassify import K_classifiers
+from pysal.inequality.theil import Theil, TheilD, TheilDSim
+from pysal.region.maxp import Maxp, Maxp_LISA
+from pysal.spatial_dynamics import Markov, Spatial_Markov, LISA_Markov, \
+        SpatialTau, Theta
+from pysal.spatial_dynamics import ergodic
+from pysal.spatial_dynamics import directional
+from pysal.weights import W, lat2W, regime_weights, comb, full, shimbel, \
+        order, higher_order, remap_ids
+from pysal.weights.Distance import knnW, Kernel, DistanceBand
+from pysal.weights.Contiguity import buildContiguity
+from pysal.weights.spatial_lag import lag_spatial
+from pysal.weights.Wsets import w_union, w_intersection, w_difference
+from pysal.weights.Wsets import w_symmetric_difference, w_subset
+from pysal.weights.user import queen_from_shapefile, rook_from_shapefile, \
+        knnW_from_array,knnW_from_shapefile, threshold_binaryW_from_array,\
+        threshold_binaryW_from_shapefile, threshold_continuousW_from_array,\
+        threshold_continuousW_from_shapefile, kernelW, kernelW_from_shapefile,\
+        adaptive_kernelW, adaptive_kernelW_from_shapefile,\
+        min_threshold_dist_from_shapefile, build_lattice_shapefile
+import pysal.spreg
+import pysal.examples
 
 # Load the IOHandlers
-from core import IOHandlers
+from pysal.core import IOHandlers
 # Assign pysal.open to dispatcher
-open = core.FileIO.FileIO
+open = pysal.core.FileIO.FileIO
 
 #__all__=[]
 #import esda,weights
