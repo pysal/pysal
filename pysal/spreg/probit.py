cd63d199d7a9f01842484d0cc32c103c50ac1d2a
Serge Rey <sjsrey@gmail.com>
2012-07-07 16:17:58 +0000

    - fixing doc string and refs

--- pysal/spreg/probit.py
+++ pysal/spreg/probit.py
@@ -91,8 +91,7 @@ class BaseProbit:
 
     References
     ----------
-    .. [1] Pinkse, J. (2004). Moran-
-flavored tests with nuisance parameter. In: Anselin,
+    .. [1] Pinkse, J. (2004). Moran-flavored tests with nuisance parameter. In: Anselin,
     L., Florax, R. J., Rey, S. J. (editors) Advances in Spatial Econometrics,
     pages 67-77. Springer-Verlag, Heidelberg.
     .. [2] Kelejian, H., Prucha, I. (2001) "On the asymptotic distribution of the
@@ -411,13 +410,9 @@ class Probit(BaseProbit):
                    
     References
     ----------
-    .. [1] Pinkse, J. (2004). Moran-flavored tests with nuisance parameter. In: Anselin,
-    L., Florax, R. J., Rey, S. J. (editors) Advances in Spatial Econometrics,
-    pages 67-77. Springer-Verlag, Heidelberg.
-    .. [2] Kelejian, H., Prucha, I. (2001) "On the asymptotic distribution of the
-    Moran I test statistic with applications". Journal of Econometrics, 104(2):219-57.
-    .. [3] Pinkse, J., Slade, M. E. (1998) "Contracting in space: an application of
-    spatial statistics to discrete-choice models". Journal of Econometrics, 85(1):125-54.
+    .. [1] Pinkse, J. (2004). Moran-flavored tests with nuisance parameter. In: Anselin, L., Florax, R. J., Rey, S. J. (editors) Advances in Spatial Econometrics, pages 67-77. Springer-Verlag, Heidelberg.
+    .. [2] Kelejian, H., Prucha, I. (2001) "On the asymptotic distribution of the Moran I test statistic with applications". Journal of Econometrics, 104(2):219-57.
+    .. [3] Pinkse, J., Slade, M. E. (1998) "Contracting in space: an application of spatial statistics to discrete-choice models". Journal of Econometrics, 85(1):125-54.
 
     Examples
     --------
