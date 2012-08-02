6db9f802c0eb3ce23b89fa500e4cab7bc109db39
Serge Rey <sjsrey@gmail.com>
2012-08-02 01:49:27 +0000

    - correct conditional this time (i hope)

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -7,7 +7,7 @@ dl = "http://code.google.com/p/pysal/downloads/list"
 def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
     pth = os.path.join(base,'examples',example_name)
-    if os.path.isfile(pth):
+    if not os.path.isfile(pth):
         print "Warning example not found: %s"%example_name
         print "PySAL examples are available at %s" % dl
-    return os.path.join(base,'examples',example_name)
+    return pth
