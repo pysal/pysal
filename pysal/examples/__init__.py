02db83093334d265e82774bdc07dcb63efb9253b
Serge Rey <sjsrey@gmail.com>
2012-08-02 00:58:41 +0000

    - link to examples download

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -3,6 +3,7 @@ import pysal
 
 __all__ = ['get_path']
 
+dl = "http://code.google.com/p/pysal/downloads/list"
 def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
     pth = os.path.join(base,'examples',example_name)
@@ -10,5 +11,5 @@ def get_path(example_name):
         return os.path.join(base,'examples',example_name)
     else:
         print "Warning example not found: %s"%example_name
-        print "PySAL examples are available at http://pysal.org"
+        print "PySAL examples are available at %s" % dl
         return None
