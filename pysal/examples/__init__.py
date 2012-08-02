bf60fda016febfcb9bb12b7ce45104f5e7284080
Serge Rey <sjsrey@gmail.com>
2012-08-02 02:32:03 +0000

    - moving back to r1049 but leaving r1310 in history for ideas on moving   forward - we need to distinguish between using examples in the doctests (which the   users see) and for the developers since we are no longer distributing   examples with the source

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -3,11 +3,6 @@ import pysal
 
 __all__ = ['get_path']
 
-dl = "http://code.google.com/p/pysal/downloads/list"
 def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
-    pth = os.path.join(base,'examples',example_name)
-    if not os.path.isfile(pth):
-        print "Warning example not found: %s"%example_name
-        print "PySAL examples are available at %s" % dl
-    return pth
+    return os.path.join(base,'examples',example_name)
