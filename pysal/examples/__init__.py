98a56a9a06a1e34816a9dbf484ee9479b4262d25
Serge Rey <sjsrey@gmail.com>
2012-08-02 00:42:55 +0000

    - explicit check if examples are actually present

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -5,4 +5,10 @@ __all__ = ['get_path']
 
 def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
-    return os.path.join(base,'examples',example_name)
+    pth = os.path.join(base,'examples',example_name)
+    if os.path.isfile(pth):
+        return os.path.join(base,'examples',example_name)
+    else:
+        print "Warning example not found: %s"%example_name
+        print "PySAL examples are available at http://pysal.org"
+        return None
