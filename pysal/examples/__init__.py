2b7cab0bf6d73ba3d4607728dc306b3843d23f42
Serge Rey <sjsrey@gmail.com>
2012-08-02 01:36:39 +0000

    - compromise - returns pth rather than None if file does not exist

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -8,8 +8,6 @@ def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
     pth = os.path.join(base,'examples',example_name)
     if os.path.isfile(pth):
-        return os.path.join(base,'examples',example_name)
-    else:
         print "Warning example not found: %s"%example_name
         print "PySAL examples are available at %s" % dl
-        return None
+    return os.path.join(base,'examples',example_name)
