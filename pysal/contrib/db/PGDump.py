60210406d14a3c6595b9b72ec5c3cdbe40543db1
Phil Stephens <phil.stphns@gmail.com>
2012-10-10 23:53:19 +0000

    - rename file - geometry type determined inline - supports all ogr filetypes - depends django, ogr

--- pysal/contrib/db/PGDump.py
+++ pysal/contrib/db/PGDump.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python
-"""
-dumps postgis database table to shapefile 
-"""
 __author__ = "Philip Stephens <philip.stephens@asu.edu "
 
 __all__ = ['db2shape']
 
+from django.contrib.gis.gdal.geomtype import OGRGeomType
 from osgeo import ogr
+import os
+
 def db2shape(connstring, input, output):
+    """
+    dumps a postgis database table to shapefile 
+
+    Arguments
+    ---------
+    connstring = A connection string with 4 parameters. Example is 
+    "PG: host='localhost' dbname='pysaldb' user='myusername'
+    password='my_password'"
+
+    input : the db table
+
+    output : a filename
+
+    Note
+    ----
+
+    If a file exists with the same name as 'output', it will be deleted
+    before being overwritten.
+    """
     conn = ogr.Open(connstring)
+    layer = conn.GetLayerByName(input)
+    type = layer.GetGeomType()  # returns an int
+    geom_type = OGRGeomType._types[type]  # map int to string description
 
     # Schema definition of SHP file
     out_driver = ogr.GetDriverByName( 'ESRI Shapefile' )
+    if os.path.exists(output):
+        out_driver.DeleteDataSource(output)
     out_ds = out_driver.CreateDataSource(output)
     out_srs = None
-    out_layer = out_ds.CreateLayer("point", out_srs, ogr.wkbPoint)
+    out_layer = out_ds.CreateLayer(geom_type, out_srs, type)
     fd = ogr.FieldDefn('name',ogr.OFTString)
     out_layer.CreateField(fd)
 
-    layer = conn.GetLayerByName(input)
     #layer = conn.ExecuteSQL(sql)
 
     feat = layer.GetNextFeature()
@@ -29,13 +52,9 @@ def db2shape(connstring, input, output):
         out_layer.CreateFeature(featDef)
         feat.Destroy()
         feat = layer.GetNextFeature()
-
     conn.Destroy()
     out_ds.Destroy()
 
 if __name__ == '__main__':
-
-    connstring = "PG: host='localhost' dbname='pysaldb' user='stephens' password='coorhall'"
-    input = 'burkitt'
-    output = "dumptest.shp"
-    db2shape(connstring, input, output)
+    import nose
+    nose.runmodule()
