932f530b1df1e21bb60c9d61df5e7c292e14f2ab
Phil Stephens <phil.stphns@gmail.com>
2012-11-05 16:27:28 +0000

    - added stub for table2gal - added README - added README for PlPython track

--- pysal/contrib/db/PGDump.py
+++ pysal/contrib/db/PGDump.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 __author__ = "Philip Stephens <philip.stephens@asu.edu "
 
-__all__ = ['db2shape']
+__all__ = ['db2shape', 'db_table2gal']
 
 from django.contrib.gis.gdal.geomtype import OGRGeomType
 from osgeo import ogr
+import pysal
 import os
 
 def db2shape(connstring, input, output):
@@ -20,6 +21,11 @@ def db2shape(connstring, input, output):
     input : the db table
 
     output : a filename
+
+    Examples
+    --------
+    TBD 
+    TODO: make pysal db tables read-only and available on network
 
     Note
     ----
@@ -54,6 +60,44 @@ def db2shape(connstring, input, output):
         feat = layer.GetNextFeature()
     conn.Destroy()
     out_ds.Destroy()
+
+def db_table2gal(connstring, input, weights_type=Contiguity):
+    """
+    generates a GAL file from a postgis database table 
+
+    Arguments
+    ---------
+    connstring = A connection string with 4 parameters. Example is 
+    "PG: host='localhost' dbname='pysaldb' user='myusername'
+    password='my_password'"
+
+    input : the db table
+
+    weights_type : type of spatial weights calculation, defaults to
+    Contiguity
+
+    Examples
+    --------
+    TBD 
+
+    Note
+    ----
+    TBD
+
+    pseudo
+    ------
+    connect to db
+    simplify
+    create topo
+    query topogeom
+    return aswkt or asgeojson
+    convert returned to pysal w 
+    write w to GAL
+
+
+    """
+    pass
+
 
 if __name__ == '__main__':
     import nose
