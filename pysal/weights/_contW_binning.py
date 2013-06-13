5616b12737fa0918781feda046a863f1e1a5e0c2
Serge Rey <sjsrey@gmail.com>
2013-06-13 11:45:18 -0700

    refactored to avoid second loop in explicit queen or rook check

--- pysal/weights/_contW_binning.py
+++ pysal/weights/_contW_binning.py
@@ -7,6 +7,8 @@ __author__ = "Sergio J. Rey <srey@asu.edu> "
 __all__ = ["QUEEN", "ROOK", "ContiguityWeights_binning"]
 
 
+import time
+
 # delta to get buckets right
 DELTA = 0.000001
 
@@ -15,15 +17,15 @@ ROOK = 2
 
 # constants for bucket sizes
 BUCK_SM = 8
-BUCK_LG = 80
+BUCK_LG =  80 
 SHP_SMALL = 1000
 
 
 def bbcommon(bb, bbother):
     """
     Checks for overlaps of bounding boxes. First, east-west, then north-south.
-    Element 0 is west, element 2 is east, element 1 is north?, element 3 is
-    south?
+    Element 0 is west, element 2 is east, element 3 is north, element 1 is
+    south.
     All four checks must be false for chflag to be true, meaning the two
     bounding boxes do not overlap.
     """
@@ -40,10 +42,9 @@ class ContiguityWeights_binning:
     def __init__(self, shpFileObject, wttype):
         self.shpFileObject = shpFileObject
         self.wttype = wttype
-        self.bucket()
-        self.doWeights()
+        self.do_weights()
 
-    def bucket(self):
+    def do_weights(self):
         shpFileObject = self.shpFileObject
 
         if shpFileObject.type != pysal.cg.Polygon:
@@ -59,18 +60,19 @@ class ContiguityWeights_binning:
             bucketmin = numPoly / BUCK_SM + 2
         else:
             bucketmin = numPoly / BUCK_LG + 2
+            #print 'bucketmin: ', bucketmin
         # bucket length
         lengthx = ((shapebox[2] + DELTA) - shapebox[0]) / bucketmin
         lengthy = ((shapebox[3] + DELTA) - shapebox[1]) / bucketmin
+
+        #print lengthx, lengthy
 
         # initialize buckets
         columns = [set() for i in range(bucketmin)]
         rows = [set() for i in range(bucketmin)]
 
-        minbox = shapebox[:2] * \
-            2                                  # minx,miny,minx,miny
-        binWidth = [lengthx, lengthy] * \
-            2                              # lenx,leny,lenx,leny
+        minbox = shapebox[:2] *  2             # minx,miny,minx,miny
+        binWidth = [lengthx, lengthy] * 2      # lenx,leny,lenx,leny
         bbcache = {}
         poly2Column = [set() for i in range(numPoly)]
         poly2Row = [set() for i in range(numPoly)]
@@ -85,102 +87,95 @@ class ContiguityWeights_binning:
             for j in range(projBBox[1], projBBox[3] + 1):
                 rows[j].add(i)
                 poly2Row[i].add(j)
-        # loop over polygons rather than bins
+
         w = {}
-        for polyId in xrange(numPoly):
-            idRows = poly2Row[polyId]
-            idCols = poly2Column[polyId]
-            rowPotentialNeighbors = set()
-            colPotentialNeighbors = set()
-            for row in idRows:
-                rowPotentialNeighbors = rowPotentialNeighbors.union(rows[row])
-            for col in idCols:
-                colPotentialNeighbors = colPotentialNeighbors.union(
-                    columns[col])
-            potentialNeighbors = rowPotentialNeighbors.intersection(
-                colPotentialNeighbors)
-            if polyId not in w:
-                w[polyId] = set()
-            for j in potentialNeighbors:
-                if polyId < j:
-                    if bbcommon(bbcache[polyId], bbcache[j]):
-                        w[polyId].add(j)
-
-        self.potentialW = w
-
-    def doWeights(self):
-        pw = self.potentialW
-        w = {}
-        shpFileObject = self.shpFileObject
-
         if self.wttype == QUEEN:
-            # check for a shared vertex
+            # loop over polygons rather than bins
             vertCache = {}
-            for polyId in xrange(self.numPoly):
-                iVerts = shpFileObject.get(polyId).vertices
+            for polyId in xrange(numPoly):
                 if polyId not in vertCache:
-                    vertCache[polyId] = set(iVerts)
-                nbrs = [j for j in pw[polyId] if j > polyId]
+                    vertCache[polyId] = set(shpFileObject.get(polyId).vertices)
+                idRows = poly2Row[polyId]
+                idCols = poly2Column[polyId]
+                rowPotentialNeighbors = set()
+                colPotentialNeighbors = set()
+                for row in idRows:
+                    rowPotentialNeighbors = rowPotentialNeighbors.union(rows[row])
+                for col in idCols:
+                    colPotentialNeighbors = colPotentialNeighbors.union(
+                        columns[col])
+                potentialNeighbors = rowPotentialNeighbors.intersection(
+                    colPotentialNeighbors)
                 if polyId not in w:
                     w[polyId] = set()
-                for j in nbrs:
-                    join = False
-                    if j not in vertCache:
-                        vertCache[j] = set(shpFileObject.get(j).vertices)
-                    common = vertCache[polyId].intersection(vertCache[j])
-                    if len(common) > 0:
-                        join = True
-                    if join:
-                        w[polyId].add(j)
-                        if j not in w:
-                            w[j] = set()
-                        w[j].add(polyId)
-            self.w = w
+                for j in potentialNeighbors:
+                    if polyId < j:
+                        if bbcommon(bbcache[polyId], bbcache[j]):
+                            if j not in vertCache:
+                                vertCache[j] = set(shpFileObject.get(j).vertices)
+                            common = vertCache[polyId].intersection(vertCache[j])
+                            if len(common) > 0:
+                                w[polyId].add(j)
+                                if j not in w:
+                                    w[j] = set()
+                                w[j].add(polyId)
         elif self.wttype == ROOK:
             # check for a shared edge
             edgeCache = {}
-            for polyId in xrange(self.numPoly):
+            # loop over polygons rather than bins
+            for polyId in xrange(numPoly):
                 if polyId not in edgeCache:
-                    iEdges ={}
+                    iEdges = {}
                     iVerts = shpFileObject.get(polyId).vertices
                     nv = len(iVerts)
                     ne = nv - 1
-                    for i in range(ne):
+                    for i in xrange(ne):
                         l = iVerts[i]
                         r = iVerts[i+1]
                         iEdges[(l,r)] = []
                         iEdges[(r,l)] = []
                     edgeCache[polyId] = iEdges
-
-                nbrs = [j for j in pw[polyId] if j > polyId]
+                iEdgeSet = set(edgeCache[polyId].keys())
+                idRows = poly2Row[polyId]
+                idCols = poly2Column[polyId]
+                rowPotentialNeighbors = set()
+                colPotentialNeighbors = set()
+                for row in idRows:
+                    rowPotentialNeighbors = rowPotentialNeighbors.union(rows[row])
+                for col in idCols:
+                    colPotentialNeighbors = colPotentialNeighbors.union(
+                        columns[col])
+                potentialNeighbors = rowPotentialNeighbors.intersection(
+                    colPotentialNeighbors)
                 if polyId not in w:
                     w[polyId] = set()
-                for j in nbrs:
-                    join = False
-                    if j not in edgeCache:
-                        jVerts = shpFileObject.get(j).vertices
-                        jEdges = {}
-                        nv = len(jVerts)
-                        ne = nv - 1
-                        for e in range(ne):
-                            l = jVerts[e]
-                            r = jVerts[e+1]
-                            jEdges[(l,r)] = []
-                            jEdges[(r,l)] = []
-                        edgeCache[j] = jEdges
-                    for edge in edgeCache[j]:
-                        if edge in edgeCache[polyId]:
-                            join = True
-                            w[polyId].add(j)
-                            if j not in w:
-                                w[j] = set()
-                            w[j].add(polyId)
-                            break
-            self.w = w
+                for j in potentialNeighbors:
+                    if polyId < j:
+                        if bbcommon(bbcache[polyId], bbcache[j]):
+                            if j not in edgeCache:
+                                jVerts = shpFileObject.get(j).vertices
+                                jEdges = {}
+                                nv = len(jVerts)
+                                ne = nv - 1
+                                for e in xrange(ne):
+                                    l = jVerts[e]
+                                    r = jVerts[e+1]
+                                    jEdges[(l,r)] = []
+                                    jEdges[(r,l)] = []
+                                edgeCache[j] = jEdges
+                            #for edge in edgeCache[j]:
+                            if iEdgeSet.intersection(edgeCache[j].keys()):
+                                w[polyId].add(j)
+                                if j not in w:
+                                    w[j] = set()
+                                w[j].add(polyId)
+                                #break
         else:
-            print 'unsupported weight type'
+            print "Unsupported weight type."
 
 
+        self.w = w
+        
 if __name__ == "__main__":
     import time
     fname = pysal.examples.get_path('NAT.shp')
@@ -194,8 +189,10 @@ if __name__ == "__main__":
     t0 = time.time()
     rb = ContiguityWeights_binning(pysal.open(fname), ROOK)
     t1 = time.time()
+    print 'Rook binning'
     print "using " + str(fname)
     print "time elapsed for rook... using bins: " + str(t1 - t0)
+
 
 
     from _contW_rtree import ContiguityWeights_rtree
