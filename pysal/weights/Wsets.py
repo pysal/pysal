335540a932a64c088a8707013539dfef02eeb3bb
darribas <daniel.arribas.bel@gmail.com>
2013-07-30 17:41:56 +0200

    silent_island_warning implemented for w_difference

--- pysal/weights/Wsets.py
+++ pysal/weights/Wsets.py
@@ -143,7 +143,7 @@ def w_intersection(w1, w2, w_shape='w1'):
     return pysal.W(neighbors)
 
 
-def w_difference(w1, w2, w_shape='w1', constrained=True):
+def w_difference(w1, w2, w_shape='w1', constrained=True, silent_island_warning=False):
     """Returns a binary weights object, w, that includes only neighbor pairs
     in w1 that are not in w2. The w_shape and constrained parameters
     determine which pairs in w1 that are not in w2 are returned.
@@ -151,21 +151,24 @@ def w_difference(w1, w2, w_shape='w1', constrained=True):
     Parameters
     ----------
 
-    w1      : W object
+    w1                      : W object
 
-    w2      : W object
+    w2                      : W object
 
-    w_shape : string
-              Defines the shape of the returned weights matrix. 'w1' returns a
-              matrix with the same IDs as w1; 'all' returns a matrix with all
-              the unique IDs from w1 and w2; and 'min' returns a matrix with
-              the IDs occurring in w1 and not in w2.
+    w_shape                 : string
+                              Defines the shape of the returned weights matrix. 'w1' returns a
+                              matrix with the same IDs as w1; 'all' returns a matrix with all
+                              the unique IDs from w1 and w2; and 'min' returns a matrix with
+                              the IDs occurring in w1 and not in w2.
 
-    constrained : boolean
-                  If False then the full set of neighbor pairs in w1 that are
-                  not in w2 are returned. If True then those pairs that would
-                  not be possible if w_shape='min' are dropped. Ignored if
-                  w_shape is set to 'min'.
+    constrained             : boolean
+                              If False then the full set of neighbor pairs in w1 that are
+                              not in w2 are returned. If True then those pairs that would
+                              not be possible if w_shape='min' are dropped. Ignored if
+                              w_shape is set to 'min'.
+    silent_island_warning   : boolean
+                              Switch to turn off (default on) print statements
+                              for every observation with islands
 
 
     Returns
@@ -239,7 +242,7 @@ def w_difference(w1, w2, w_shape='w1', constrained=True):
             neighbors[i] = list(
                 set(neighbors[i]).intersection(constrained_keys))
 
-    return pysal.W(neighbors)
+    return pysal.W(neighbors, silent_island_warning=silent_island_warning)
 
 
 def w_symmetric_difference(w1, w2, w_shape='all', constrained=True, silent_island_warning=False):
