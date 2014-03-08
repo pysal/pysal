818534f745dd1766931e939d99f5393a34246224
Serge Rey <sjsrey@gmail.com>
2014-03-07 22:44:41 -0700

    ENH stub for #266

---
+++ pysal/spatial_dynamics/rankMarkov.py
@@ -0,0 +1,19 @@
+"""
+Rank-based Markov chains for spatial distribution
+dynamics.
+"""
+
+_author_ = "Serge Rey <sjsrey@gmail.com>, Wang Sizhe <wsizhe@asu.edu>"
+
+
+class FullRankMarkov:
+    """ """
+    def __init__(self):
+        pass
+
+
+class GeographicRankMarkov:
+    """ """
+    def __init__(self):
+
+
