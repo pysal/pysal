a20fc81f45d845e27129d67dceada628828a8904
Jay <jlaura@asu.edu>
2015-01-26 13:38:23 -0700

    inline with numpydoc spec

--- pysal/esda/getisord.py
+++ pysal/esda/getisord.py
@@ -28,7 +28,7 @@ class G:
     y             : array
                     original variable
     w             : W
-                   DistanceBand W spatial weights based on distance band
+                    DistanceBand W spatial weights based on distance band
     permutation   : int
                     the number of permutations
     G             : float
@@ -41,7 +41,8 @@ class G:
                     standard normal test statistic
     p_norm        : float
                     p-value under normality assumption (one-sided)
-    sim           : array (if permutations > 0)
+    sim           : array
+                    (if permutations > 0)
                     vector of G values for permutated samples
     p_sim         : float
                     p-value based on permutations (one-sided)
@@ -165,18 +166,17 @@ class G_Local:
     Parameters
     ----------
     y : array
-       variable
+        variable
     w : DistanceBand W
-       weights instance that is based on threshold distance
-       and is assumed to be aligned with y
-    transform : string
+        weights instance that is based on threshold distance
+        and is assumed to be aligned with y
+    transform : {'R', B'}
                 the type of w, either 'B' (binary) or 'R' (row-standardized)
     permutations : int
                   the number of random permutations for calculating
                   pseudo p values
-    star : boolean
-          whether or not to include focal observation in sums
-          default is False
+    star : {False, True}
+           whether or not to include focal observation in sums
 
     Attributes
     ----------
@@ -185,38 +185,38 @@ class G_Local:
     w: DistanceBand W
        original weights object
     permutations: int
-                 the number of permutations
-    Gs: array of floats
-        the value of the orginal G statistic in Getis & Ord (1992)
+                  the number of permutations
+    Gs: array
+        of floats, the value of the orginal G statistic in Getis & Ord (1992)
     EGs: float
          expected value of Gs under normality assumption
          the values is scalar, since the expectation is identical
          across all observations
-    VGs: array of floats
-         variance values of Gs under normality assumption
-    Zs: array of floats
-        standardized Gs
-    p_norm: array of floats
-            p-value under normality assumption (one-sided)
+    VGs: array
+         of floats, variance values of Gs under normality assumption
+    Zs: array
+        of floats, standardized Gs
+    p_norm: array
+            of floats, p-value under normality assumption (one-sided)
             for two-sided tests, this value should be multiplied by 2
-    sim: array of arrays of floats (if permutations>0)
-         vector of I values for permutated samples
-    p_sim: array of floats
-           p-value based on permutations (one-sided)
+    sim: array
+         of arrays of floats (if permutations>0), vector of I values
+         for permutated samples
+    p_sim: array
+           of floats, p-value based on permutations (one-sided)
            null - spatial randomness
            alternative - the observed G is extreme it is either extremely high or extremely low
-    EG_sim: array of floats
-            average value of G from permutations
-    VG_sim: array of floats
-            variance of G from permutations
-    seG_sim: array of floats
-             standard deviation of G under permutations.
-    z_sim: array of floats
-           standardized G based on permutations
-    p_z_sim: array of floats
-             p-value based on standard normal approximation from
+    EG_sim: array
+            of floats, average value of G from permutations
+    VG_sim: array
+            of floats, variance of G from permutations
+    seG_sim: array
+             of floats, standard deviation of G under permutations.
+    z_sim: array
+           of floats, standardized G based on permutations
+    p_z_sim: array
+             of floats, p-value based on standard normal approximation from
              permutations (one-sided)
-
     Notes
     -----
     To compute moments of Gs under normality assumption,
