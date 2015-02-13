a7554aca611fcf52c150f2d2e17ed625dfd062bc
Levi John Wolf <ljw@auss.dhcp.asu.edu>
2015-02-12 22:30:35 -0700

    listing examples

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -1,9 +1,17 @@
 import os
 import pysal
 
-__all__ = ['get_path']
+__all__ = ['get_path', 'list_examples']
 
 
 def get_path(example_name):
     base = os.path.split(pysal.__file__)[0]
     return os.path.join(base, 'examples', example_name)
+
+def list_examples():
+    base = os.path.split(pysal.__file__)[0]
+    files = os.listdir(os.path.join(base, 'examples'))
+    exs = set([os.path.splitext(x)[0] for x in flist])
+    exs.remove('README')
+    exs.remove('__init__')
+    return uflist
