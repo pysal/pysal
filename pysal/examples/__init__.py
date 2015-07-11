c569485397ee08bc1f113f5b3fc9b2a84f34fb89
Serge Rey <sjsrey@gmail.com>
2015-07-11 15:58:23 -0500

    adjusting for new example organization

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -3,11 +3,11 @@ import pysal
 
 __all__ = ['get_path']
 
-
 base = os.path.split(pysal.__file__)[0]
 example_dir = os.path.join(base,"examples")
 dirs = next(os.walk(example_dir))[1]
 file_2_dir = {}
+print dirs
 
 for d in dirs:
     tmp = os.path.join(example_dir, d)
@@ -16,8 +16,24 @@ for d in dirs:
         file_2_dir[f] = tmp
 
 def get_path(example_name):
-    if example_name in file_2_dir:
+    if example_name in dirs:
+        return os.path.join(example_dir,example_name, example_name)
+    elif example_name in file_2_dir:
         d = file_2_dir[example_name]
         return os.path.join(d, example_name)
+    elif example_name == "":
+        return os.path.join(base,'examples', example_name)
     else:
         print(example_name+ ' not found in PySAL built-in examples.')
+
+"""
+import os
+import pysal
+
+__all__ = ['get_path']
+
+
+def get_path(example_name):
+        base = os.path.split(pysal.__file__)[0]
+            return os.path.join(base, 'examples', example_name)
+"""
