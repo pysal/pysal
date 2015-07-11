c327d4d9ab46d122737fcaa7eeb8dd1e881793ba
Serge Rey <sjsrey@gmail.com>
2015-07-11 11:39:27 -0500

    fixing paths

--- pysal/examples/__init__.py
+++ pysal/examples/__init__.py
@@ -5,19 +5,19 @@ __all__ = ['get_path']
 
 
 base = os.path.split(pysal.__file__)[0]
-dirs = next(os.walk('.'))[1][2:]
+example_dir = os.path.join(base,"examples")
+dirs = next(os.walk(example_dir))[1][2:]
 file_2_dir = {}
 
 for d in dirs:
-    tmp = os.path.join(base,'examples',d)
+    tmp = os.path.join(example_dir, d)
     files_in_tmp = os.listdir(tmp)
-    for file in files_in_tmp:
-        file_2_dir[file] = tmp
-
+    for f in files_in_tmp:
+        file_2_dir[f] = tmp
 
 def get_path(example_name):
     if example_name in file_2_dir:
         d = file_2_dir[example_name]
         return os.path.join(d, example_name)
     else:
-        print(example_name+ ' not found in examples.')
+        print(example_name+ ' not found in PySAL built-in examples.')
