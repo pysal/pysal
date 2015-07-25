9377a1d482f12534ba7555227a9b69817e1569fc
ljwolf <ljw2@asu.edu>
2015-07-24 22:11:34 -0700

    add python3 for travis detection??

--- setup.py
+++ setup.py
@@ -107,7 +107,8 @@ def setup_package():
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.5',
             'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7'
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4'
         ],
         packages=find_packages(exclude=[".meta", "*.meta.*", "meta.*",
                                         "meta"]),
