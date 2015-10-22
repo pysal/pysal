11645657409a06d54a324a7cb6b7f755a73ae278
levi.john.wolf@gmail.com <ljw2@asu.edu>
2015-10-21 21:33:26 -0700

    adding handler discussion notebook

--- pysal/contrib/handler/globalscheck.py
+++
@@ -1,6 +0,0 @@
-g1 = globals().keys()
-from registry import *
-g2 = globals().keys()
-additions = [k for k in g2 if k not in g1]
-
-print(additions)
--- pysal/contrib/handler/todo.md
+++
@@ -1,2 +0,0 @@
-Fix summary work
-Figure out why GM_Error_Hom_regimes doesn't have a mean_y to work wit
