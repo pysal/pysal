ca08160a58bf00fb3e269fa41d76213fcaa5f105
Serge Rey <sjsrey@gmail.com>
2015-12-05 15:59:59 -0700

    merge

--- pysal/spreg/tests/test_error_sp_regimes.py
+++ pysal/spreg/tests/test_error_sp_regimes.py
@@ -4,6 +4,7 @@ import pysal
 import numpy as np
 from pysal.spreg import error_sp_regimes as SP
 from pysal.spreg.error_sp import GM_Error, GM_Endog_Error, GM_Combo
+from pysal.common import RTOL
 
 @unittest.skipIf(int(scipy.__version__.split(".")[1]) < 11,
 "Maximum Likelihood requires SciPy version 11 or newer.")
@@ -53,41 +54,41 @@ class TestGM_Error_Regimes(unittest.TestCase):
        [ -0.33550084],
        [ -0.85076108],
        [  0.38671608]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         u = np.array([-2.06177251])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([ 17.78775251])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         n = 49
-        self.assertAlmostEqual(reg.n,n,4)
+        np.testing.assert_allclose(reg.n,n,RTOL)
         k = 6
-        self.assertAlmostEqual(reg.k,k,4)
+        np.testing.assert_allclose(reg.k,k,RTOL)
         y = np.array([ 15.72598])
-        np.testing.assert_array_almost_equal(reg.y[0],y,4)
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
         x = np.array([[  0.      ,   0.      ,   0.      ,   1.      ,  80.467003,  19.531   ]])
-        np.testing.assert_array_almost_equal(reg.x[0].toarray(),x,4)
+        np.testing.assert_allclose(reg.x[0].toarray(),x,RTOL)
         e = np.array([ 1.40747232])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e,RTOL)
         my = 35.128823897959187
-        self.assertAlmostEqual(reg.mean_y,my)
+        np.testing.assert_allclose(reg.mean_y,my,RTOL)
         sy = 16.732092091229699
-        self.assertAlmostEqual(reg.std_y,sy)
+        np.testing.assert_allclose(reg.std_y,sy,RTOL)
         vm = np.array([ 50.55875289,  -0.14444487,  -2.05735489,   0.        ,
          0.        ,   0.        ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         sig2 = 102.13050615267227
-        self.assertAlmostEqual(reg.sig2,sig2,4)
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)
         pr2 = 0.5525102200608539
-        self.assertAlmostEqual(reg.pr2,pr2)
+        np.testing.assert_allclose(reg.pr2,pr2,RTOL)
         std_err = np.array([ 7.11046784,  0.21879293,  0.58477864,  7.50596504,  0.10800686,
         0.57365981])
-        np.testing.assert_array_almost_equal(reg.std_err,std_err,4)
+        np.testing.assert_allclose(reg.std_err,std_err,RTOL)
         chow_r = np.array([[ 0.03533785,  0.85088948],
        [ 0.54918491,  0.45865093],
        [ 0.67115641,  0.41264872]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 0.81985446000130979
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j,RTOL)
     """
     def test_model_regi_error(self):
         #Columbus:
@@ -100,80 +101,82 @@ class TestGM_Error_Regimes(unittest.TestCase):
        [ -0.31845995],
        [ -1.29047149],
        [  0.08092997]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         vm = np.array([ 39.33656288,  -0.08420799,  -1.50350999,   0.        ,
          0.        ,   0.        ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         u = np.array([ 0.00698341])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([ 15.71899659])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         e = np.array([ 0.53685671])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e,RTOL)
         chow_r = np.array([[  3.63674458e-01,   5.46472584e-01],
        [  4.29607250e-01,   5.12181727e-01],
        [  5.44739543e-04,   9.81379339e-01]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 0.70119418251625387
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j,4)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j,RTOL)
         #Artficial:
         model = SP.GM_Error_Regimes(self.y_a, self.x_a, self.regi_a, w=self.w_a, regime_err_sep=True)
         model1 = GM_Error(self.y_a[0:(self.n2)].reshape((self.n2),1), self.x_a[0:(self.n2)], w=self.w_a1)
         model2 = GM_Error(self.y_a[(self.n2):].reshape((self.n2),1), self.x_a[(self.n2):], w=self.w_a1)
         tbetas = np.vstack((model1.betas, model2.betas))
-        np.testing.assert_array_almost_equal(model.betas,tbetas)
+        np.testing.assert_allclose(model.betas,tbetas,RTOL)
         vm = np.hstack((model1.vm.diagonal(),model2.vm.diagonal()))
-        np.testing.assert_array_almost_equal(model.vm.diagonal(), vm, 4)
+        np.testing.assert_allclose(model.vm.diagonal(), vm,RTOL)
     """
     def test_model_endog(self):
         reg = SP.GM_Endog_Error_Regimes(self.y, self.X1, self.yd, self.q, self.regimes, self.w)
         betas = np.array([[ 77.48385551,   4.52986622,  78.93209405,   0.42186261,
          -3.23823854,  -1.1475775 ,   0.20222108]])
-        np.testing.assert_array_almost_equal(reg.betas.T,betas,4)
+        print('Runining higher-tolerance test on L133 of test_error_sp_regimes.py')
+        np.testing.assert_allclose(reg.betas.T,betas,RTOL + .0001)
         u = np.array([ 20.89660904])
-        #np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        #np.testing.assert_allclose(reg.u[0],u,RTOL)
         np.testing.assert_allclose(reg.u[0],u,rtol=1e-05)
         e = np.array([ 25.21818724])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e,RTOL)
         predy = np.array([-5.17062904])
-        #np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        #np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         np.testing.assert_allclose(reg.predy[0],predy,rtol=1e-03)
         n = 49
-        self.assertAlmostEqual(reg.n,n)
+        np.testing.assert_allclose(reg.n,n)
         k = 6
-        self.assertAlmostEqual(reg.k,k)
+        np.testing.assert_allclose(reg.k,k)
         y = np.array([ 15.72598])
-        np.testing.assert_array_almost_equal(reg.y[0],y,4)
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
         x = np.array([[  0.   ,   0.   ,   1.   ,  19.531]])
-        np.testing.assert_array_almost_equal(reg.x[0].toarray(),x,4)
+        np.testing.assert_allclose(reg.x[0].toarray(),x,RTOL)
         yend = np.array([[  0.      ,  80.467003]])
-        np.testing.assert_array_almost_equal(reg.yend[0].toarray(),yend,4)
+        np.testing.assert_allclose(reg.yend[0].toarray(),yend,RTOL)
         z = np.array([[  0.      ,   0.      ,   1.      ,  19.531   ,   0.      ,
          80.467003]])
-        np.testing.assert_array_almost_equal(reg.z[0].toarray(),z,4)
+        np.testing.assert_allclose(reg.z[0].toarray(),z,RTOL)
         my = 35.128823897959187
-        self.assertAlmostEqual(reg.mean_y,my)
+        np.testing.assert_allclose(reg.mean_y,my)
         sy = 16.732092091229699
-        self.assertAlmostEqual(reg.std_y,sy)
+        np.testing.assert_allclose(reg.std_y,sy)
         vm = np.array([ 390.88250241,   52.25924084,    0.        ,    0.        ,
         -32.64274729,    0.        ])
-        #np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        #np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         np.allclose(reg.vm, vm)
         pr2 = 0.19623994206233333
-        self.assertAlmostEqual(reg.pr2,pr2,4)
+        np.testing.assert_allclose(reg.pr2,pr2,RTOL)
         sig2 = 649.4011
-        #self.assertAlmostEqual(round(reg.sig2,4),round(sig2,4),4)
+        #np.testing.assert_allclose(round(reg.sig2,RTOL),round(sig2,RTOL),RTOL)
         np.testing.assert_allclose(sig2, reg.sig2, rtol=1e-05)
         std_err = np.array([ 19.77074866,   6.07667394,  24.32254786,   2.17776972,
          2.97078606,   0.94392418])
-        #np.testing.assert_array_almost_equal(reg.std_err,std_err,4)
+        #np.testing.assert_allclose(reg.std_err,std_err,RTOL)
         np.testing.assert_allclose(reg.std_err, std_err, rtol=1e-05)
         chow_r = np.array([[ 0.0021348 ,  0.96314775],
        [ 0.40499741,  0.5245196 ],
        [ 0.4498365 ,  0.50241261]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        print('Running higher-tolerance tests on L176 of test_error_sp_regimes.py')
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL+.0001)
         chow_j = 1.2885590185243503
-        #self.assertAlmostEqual(reg.chow.joint[0],chow_j)
+        #np.testing.assert_allclose(reg.chow.joint[0],chow_j)
         np.testing.assert_allclose(reg.chow.joint[0], chow_j, rtol=1e-05)
 
     def test_model_endog_regi_error(self):
@@ -187,36 +190,36 @@ class TestGM_Error_Regimes(unittest.TestCase):
        [  5.68908920e-01],
        [ -1.28824699e+00],
        [  6.70387351e-02]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         vm = np.array([ 791.86679123,  140.12967794,  -81.37581255,    0.        ,
           0.        ,    0.        ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         u = np.array([ 25.80361497])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([-10.07763497])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         e = np.array([ 27.32251813])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e,RTOL)
         chow_r = np.array([[ 0.00926459,  0.92331985],
        [ 0.26102777,  0.60941494],
        [ 0.26664581,  0.60559072]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 1.1184631131987004
-        #self.assertAlmostEqual(reg.chow.joint[0],chow_j)
-        np.testing.assert_allclose(reg.chow.joint[0], chow_j)
+        #np.testing.assert_allclose(reg.chow.joint[0],chow_j)
+        np.testing.assert_allclose(reg.chow.joint[0], chow_j,RTOL)
         #Artficial:
         model = SP.GM_Endog_Error_Regimes(self.y_a, self.x_a1, yend=self.x_a2, q=self.q_a, regimes=self.regi_a, w=self.w_a, regime_err_sep=True)
         model1 = GM_Endog_Error(self.y_a[0:(self.n2)].reshape((self.n2),1), self.x_a1[0:(self.n2)], yend=self.x_a2[0:(self.n2)], q=self.q_a[0:(self.n2)], w=self.w_a1)
         model2 = GM_Endog_Error(self.y_a[(self.n2):].reshape((self.n2),1), self.x_a1[(self.n2):], yend=self.x_a2[(self.n2):], q=self.q_a[(self.n2):], w=self.w_a1)
         tbetas = np.vstack((model1.betas, model2.betas))
-        np.testing.assert_array_almost_equal(model.betas,tbetas)
+        np.testing.assert_allclose(model.betas,tbetas,RTOL)
         vm = np.hstack((model1.vm.diagonal(),model2.vm.diagonal()))
-        np.testing.assert_array_almost_equal(model.vm.diagonal(), vm, 4)
+        np.testing.assert_allclose(model.vm.diagonal(), vm,RTOL)
 
     def test_model_combo(self):
         reg = SP.GM_Combo_Regimes(self.y, self.X1, self.regimes, self.yd, self.q, w=self.w)
         predy_e = np.array([ 18.82774339])
-        np.testing.assert_array_almost_equal(reg.predy_e[0],predy_e,4)
+        np.testing.assert_allclose(reg.predy_e[0],predy_e,RTOL)
         betas = np.array([[ 36.44798052],
        [ -0.7974482 ],
        [ 30.53782661],
@@ -225,48 +228,48 @@ class TestGM_Error_Regimes(unittest.TestCase):
        [ -0.21736652],
        [  0.64801059],
        [ -0.16601265]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         u = np.array([ 0.84393304])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         e_filtered = np.array([ 0.4040027])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e_filtered,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e_filtered,RTOL)
         predy = np.array([ 14.88204696])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         n = 49
-        self.assertAlmostEqual(reg.n,n)
+        np.testing.assert_allclose(reg.n,n,RTOL)
         k = 7
-        self.assertAlmostEqual(reg.k,k)
+        np.testing.assert_allclose(reg.k,k,RTOL)
         y = np.array([ 15.72598])
-        np.testing.assert_array_almost_equal(reg.y[0],y,4)
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
         x = np.array([[  0.   ,   0.   ,   1.   ,  19.531]])
-        np.testing.assert_array_almost_equal(reg.x[0].toarray(),x,4)
+        np.testing.assert_allclose(reg.x[0].toarray(),x,RTOL)
         yend = np.array([[  0.       ,  80.467003 ,  24.7142675]])
-        np.testing.assert_array_almost_equal(reg.yend[0].toarray(),yend,4)
+        np.testing.assert_allclose(reg.yend[0].toarray(),yend,RTOL)
         z = np.array([[  0.       ,   0.       ,   1.       ,  19.531    ,   0.       ,
          80.467003 ,  24.7142675]])
-        np.testing.assert_array_almost_equal(reg.z[0].toarray(),z,4)
+        np.testing.assert_allclose(reg.z[0].toarray(),z,RTOL)
         my = 35.128823897959187
-        self.assertAlmostEqual(reg.mean_y,my)
+        np.testing.assert_allclose(reg.mean_y,my,RTOL)
         sy = 16.732092091229699
-        self.assertAlmostEqual(reg.std_y,sy)
+        np.testing.assert_allclose(reg.std_y,sy,RTOL)
         vm = np.array([ 109.23549239,   -0.19754121,   84.29574673,   -1.99317178,
          -1.60123994,   -0.1252719 ,   -1.3930344 ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         sig2 = 94.98610921110007
-        self.assertAlmostEqual(reg.sig2,sig2,4)
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)
         pr2 = 0.6493586702255537
-        self.assertAlmostEqual(reg.pr2,pr2)
+        np.testing.assert_allclose(reg.pr2,pr2,RTOL)
         pr2_e = 0.5255332447240576
-        self.assertAlmostEqual(reg.pr2_e,pr2_e)
+        np.testing.assert_allclose(reg.pr2_e,pr2_e,RTOL)
         std_err = np.array([ 10.45157846,   0.93942923,  11.38484969,   0.60774708,
          0.44461334,   0.15871227,   0.15738141])
-        np.testing.assert_array_almost_equal(reg.std_err,std_err,4)
+        np.testing.assert_allclose(reg.std_err,std_err,RTOL)
         chow_r = np.array([[ 0.49716076,  0.48075032],
        [ 0.00405377,  0.94923363],
        [ 0.03866684,  0.84411016]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 0.64531386285872072
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j,RTOL)
 
     def test_model_combo_regi_error(self):
         #Columbus:
@@ -281,29 +284,29 @@ class TestGM_Error_Regimes(unittest.TestCase):
        [ -0.48972903],
        [  0.65883658],
        [ -0.17174845]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         vm = np.array([ 153.58614432,    2.96302131,   -3.26211855,   -2.46914703,
           0.        ,    0.        ,    0.        ,    0.        ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         u = np.array([ 7.73968703])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([ 7.98629297])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         e = np.array([ 6.45052714])
-        np.testing.assert_array_almost_equal(reg.e_filtered[0],e,4)
+        np.testing.assert_allclose(reg.e_filtered[0],e,RTOL)
         chow_r = np.array([[  1.00886404e-01,   7.50768497e-01],
        [  3.61843271e-05,   9.95200481e-01],
        [  4.69585772e-02,   8.28442711e-01],
        [  8.13275259e-02,   7.75506385e-01]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 0.28479988992843119
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j, RTOL)
         #Artficial:
         model = SP.GM_Combo_Regimes(self.y_a, self.x_a1, yend=self.x_a2, q=self.q_a, regimes=self.regi_a, w=self.w_a, regime_err_sep=True, regime_lag_sep=True)
         model1 = GM_Combo(self.y_a[0:(self.n2)].reshape((self.n2),1), self.x_a1[0:(self.n2)], yend=self.x_a2[0:(self.n2)], q=self.q_a[0:(self.n2)], w=self.w_a1)
         model2 = GM_Combo(self.y_a[(self.n2):].reshape((self.n2),1), self.x_a1[(self.n2):], yend=self.x_a2[(self.n2):], q=self.q_a[(self.n2):], w=self.w_a1)
         tbetas = np.vstack((model1.betas, model2.betas))
-        np.testing.assert_array_almost_equal(model.betas,tbetas)
+        np.testing.assert_allclose(model.betas,tbetas)
         vm = np.hstack((model1.vm.diagonal(),model2.vm.diagonal()))
 
 if __name__ == '__main__':
--- pysal/spreg/tests/test_ml_lag_regimes.py
+++ pysal/spreg/tests/test_ml_lag_regimes.py
@@ -5,6 +5,7 @@ import numpy as np
 from pysal.spreg.ml_lag_regimes import ML_Lag_Regimes
 from pysal.spreg.ml_lag import ML_Lag
 from pysal.spreg import utils
+from pysal.common import RTOL
 
 @unittest.skipIf(int(scipy.__version__.split(".")[1]) < 11,
          "Max Likelihood requires SciPy version 11 or newer.")
@@ -36,49 +37,49 @@ class TestMLError(unittest.TestCase):
        [ -0.17021393],
        [  0.81941371],
        [  0.53850323]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         u = np.array([ 32.73718478])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([ 14.26281522])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         n = 211
-        self.assertAlmostEqual(reg.n,n,4)
+        np.testing.assert_allclose(reg.n,n,RTOL)
         k = 9
-        self.assertAlmostEqual(reg.k,k,4)
+        np.testing.assert_allclose(reg.k,k,RTOL)
         y = np.array([ 47.])
-        np.testing.assert_array_almost_equal(reg.y[0],y,4)
+        np.testing.assert_allclose(reg.y[0],y,RTOL)
         x = np.array([[   1.  ,    4.  ,  148.  ,   11.25,    0.  ,    0.  ,    0.  ,
            0.  ]])
-        np.testing.assert_array_almost_equal(reg.x[0].toarray(),x,4)
+        np.testing.assert_allclose(reg.x[0].toarray(),x,RTOL)
         e = np.array([ 29.45407124])
-        np.testing.assert_array_almost_equal(reg.e_pred[0],e,4)
+        np.testing.assert_allclose(reg.e_pred[0],e,RTOL)
         my = 44.307180094786695
-        self.assertAlmostEqual(reg.mean_y,my)
+        np.testing.assert_allclose(reg.mean_y,my)
         sy = 23.606076835380495
-        self.assertAlmostEqual(reg.std_y,sy)
+        np.testing.assert_allclose(reg.std_y,sy)
         vm = np.array([ 47.42000914,   2.39526578,   0.00506895,   0.06480022,
         69.67653371,   3.20661492,   0.01156766,   0.04862014,   0.00400775])
-        np.testing.assert_array_almost_equal(reg.vm.diagonal(),vm,4)
+        np.testing.assert_allclose(reg.vm.diagonal(),vm,RTOL)
         sig2 = 200.04433357145007
-        self.assertAlmostEqual(reg.sig2,sig2,4)
+        np.testing.assert_allclose(reg.sig2,sig2,RTOL)
         pr2 = 0.6404460298085746
-        self.assertAlmostEqual(reg.pr2,pr2)
+        np.testing.assert_allclose(reg.pr2,pr2)
         std_err = np.array([ 6.88621878,  1.54766462,  0.07119654,  0.25455888,  8.34724707,
         1.79070235,  0.10755305,  0.22049975,  0.0633068 ])
-        np.testing.assert_array_almost_equal(reg.std_err,std_err,4)
+        np.testing.assert_allclose(reg.std_err,std_err,RTOL)
         logll = -864.98505596489736
-        self.assertAlmostEqual(reg.logll,logll,4)
+        np.testing.assert_allclose(reg.logll,logll,RTOL)
         aic = 1747.9701119297947
-        self.assertAlmostEqual(reg.aic,aic,4)
+        np.testing.assert_allclose(reg.aic,aic,RTOL)
         schwarz = 1778.1368351310794
-        self.assertAlmostEqual(reg.schwarz,schwarz,4)
+        np.testing.assert_allclose(reg.schwarz,schwarz,RTOL)
         chow_r = np.array([[ 1.00180776,  0.31687348],
        [ 0.05904944,  0.8080047 ],
        [ 1.16987812,  0.27942629],
        [ 1.95931177,  0.16158694]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 21.648337464039283
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j,4)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j,RTOL)
 
     def test_model2(self):
         reg = ML_Lag_Regimes(self.y,self.x,self.regimes,w=self.w,name_y=self.y_name,name_x=self.x_names,\
@@ -93,25 +94,25 @@ class TestMLError(unittest.TestCase):
        [-0.18207394],
        [ 0.71129227],
        [ 0.66753263]])
-        np.testing.assert_array_almost_equal(reg.betas,betas,4)
+        np.testing.assert_allclose(reg.betas,betas,RTOL)
         vm = np.array([ 55.3593679 ,  -7.22927797,  -0.19487326,   0.6030953 ,
         -0.52249569,   0.        ,   0.        ,   0.        ,
          0.        ,   0.        ])
-        np.testing.assert_array_almost_equal(reg.vm[0],vm,4)
+        np.testing.assert_allclose(reg.vm[0],vm,RTOL)
         u = np.array([ 34.03630518])
-        np.testing.assert_array_almost_equal(reg.u[0],u,4)
+        np.testing.assert_allclose(reg.u[0],u,RTOL)
         predy = np.array([ 12.96369482])
-        np.testing.assert_array_almost_equal(reg.predy[0],predy,4)
+        np.testing.assert_allclose(reg.predy[0],predy,RTOL)
         e = np.array([ 32.46466912])
-        np.testing.assert_array_almost_equal(reg.e_pred[0],e,4)
+        np.testing.assert_allclose(reg.e_pred[0],e,RTOL)
         chow_r = np.array([[  0.15654726,   0.69235548],
        [  0.43533847,   0.509381  ],
        [  0.60552514,   0.43647766],
        [  0.59214981,   0.441589  ],
        [ 11.69437282,   0.00062689]])
-        np.testing.assert_array_almost_equal(reg.chow.regi,chow_r,4)
+        np.testing.assert_allclose(reg.chow.regi,chow_r,RTOL)
         chow_j = 21.978012275873063
-        self.assertAlmostEqual(reg.chow.joint[0],chow_j,4)
+        np.testing.assert_allclose(reg.chow.joint[0],chow_j,RTOL)
 
 if __name__ == '__main__':
     unittest.main()
