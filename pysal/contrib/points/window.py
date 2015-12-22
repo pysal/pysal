a363b24d1082554878d5bf1074092b539d10cfc6
Serge Rey <sjsrey@gmail.com>
2015-12-22 09:37:39 -0800

    helper method for making window from ps polygon

---
+++ pysal/contrib/points/window.py
@@ -0,0 +1,26 @@
+import pysal as ps
+import numpy as np
+
+
+def poly_from_bbox(bbox):
+    l, b, r, t = bbox
+    c = [(l, b), (l, t), (r, t), (r, b), (l, b)]
+    return ps.cg.shapes.Polygon(c)
+
+
+def as_window(pysal_polygon):
+    if pysal_polygon.holes == [[]]:
+        return Window(pysal_polygon.parts)
+    else:
+        return Window(pysal_polygon.parts, pysal_polygon.holes)
+
+
+class Window(ps.cg.shapes.Polygon):
+    def __init__(self, parts, holes=[]):
+        if holes:
+            super(Window, self).__init__(parts, holes)
+        else:
+            super(Window, self).__init__(parts)
+
+    def filter_contained(self, points):
+        return [np.asarray(pnt) for pnt in points if self.contains_point(pnt)]
