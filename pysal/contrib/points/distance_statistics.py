7766e880de9b57c6e9dbf85419369149c85caeef
Serge Rey <sjsrey@gmail.com>
2015-12-22 13:18:12 -0800

    f and f_envelopes

--- pysal/contrib/points/distance_statistics.py
+++ pysal/contrib/points/distance_statistics.py
@@ -33,3 +33,62 @@ def g_envelopes(pp, intervals=10, d=None, reps=99, pct=0.05):
     mean = gs.mean(axis=0)
 
     return [gs, x, gobs, mean, low, high]
+
+
+def f(pp, n=100, intervals=10, dmin=0.0, dmax=None, d=None):
+    """
+    F: empty space function
+
+    Arguments
+    ---------
+    n: int
+       number of empty space points
+    intevals: int
+        number of intervals to evalue F over
+    dmin: float
+           lower limit of distance range
+    dmax: float
+           upper limit of distance range
+           if dmax is None dmax will be set to maxnnd
+    d:   array-like
+         domain for F function
+
+    Returns
+    -------
+    cdf: array (intervals x 2)
+         first column is d, second column is cdf(d)
+
+    """
+
+    # get a csr pattern in window of pp
+    c = csr(pp.window, n, 1, asPP=True).realizations[0]
+    # for each point in csr pattern find the closest point in pp and the
+    # associated distance
+    nnids, nnds = pp.knn_other(c, k=1)
+
+    if d is None:
+        w = pp.max_nnd/intervals
+        if dmax:
+            w = dmax/intervals
+        d = [w*i for i in range(intervals + 2)]
+    cdf = [0] * len(d)
+
+    for i, d_i in enumerate(d):
+        smaller = [nndi for nndi in nnds if nndi <= d_i]
+        cdf[i] = len(smaller)*1./n
+    return np.vstack((d, cdf)).T
+
+
+def f_envelopes(pp, intervals=10, d=None, reps=99, pct=0.05):
+    obs = f(pp, intervals=intervals, d=d)
+    sim = csr(pp.window, pp.n, reps, asPP=True)
+    fs = np.asarray([f(p, d=obs[:, 0]) for p in sim.realizations.values()])
+    fs = fs[:, :, -1]
+    fs.sort(axis=0)
+    low = fs[np.int(reps * pct)]
+    high = fs[np.int(reps * (1-pct))]
+    x = obs[:, 0]
+    fobs = obs[:, 1]
+    mean = fs.mean(axis=0)
+
+    return [fs, x, fobs, mean, low, high]
