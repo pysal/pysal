beace6676894fa61fd6a1328b8b0cc08e1fd8b49
Marynia <marynia.kolak@gmail.com>
2016-05-17 17:55:31 -0500

    Update plot.py

--- pysal/contrib/viz/plot.py
+++ pysal/contrib/viz/plot.py
@@ -1,6 +1,5 @@
 """
-Moran Plot using PySAL and Matplotlib
-
+Canned Views using PySAL and Matplotlib
 """
 
 __author__ = "Marynia Kolak <marynia.kolak@gmail.com>"
@@ -15,42 +14,33 @@ def moran(var, w, xlabel='', ylabel='', title='', custom=(5,5)):
 	'''
     Produce basic Moran Plot 
     ...
-
     Arguments
     ---------
-
-    var	         	: array
+    var	            : array
                       values of variable
-    w       	    : array
-                      values of spatial weight
+    w       	    : spatial weight
     '''
 
-	
-	## really shouldn't have w transformed here -- error if doesn't match size? (MK)
-	w.transform = 'r' 
+	w.transform = 'r'
 	slag = ps.lag_spatial(w, var)
 
-	## Z-Score standardisation -- again, how to grab zx and zy from Moran call? (MK)
-	y_std   = (var - var.mean())/var.std()
-	yl_std  = (slag - slag.mean())/slag.std()
+	zx   = (var - var.mean())/var.std()
+	zy  = (slag - slag.mean())/slag.std()
+
+	fit = ps.spreg.OLS(zx[:, None], zy[:,None])
 
 	## Customize plot
 	fig1 = plt.figure(figsize=custom)
 	plt.xlabel(xlabel, fontsize=20)
-    	plt.ylabel(ylabel, fontsize=20)
-    	plt.suptitle(title, fontsize=30)
+	plt.ylabel(ylabel, fontsize=20)
+	plt.suptitle(title, fontsize=30)
 
-	plt.scatter(y_std, yl_std, s=60, color='k', alpha=.6)
-	
-	## Add Moran line here -- how much do we assume has been done so far?
+	plt.scatter(zx, zy, s=60, color='k', alpha=.6)
+	plot(zy, fit.predy, color='r')
 
 	plt.axvline(0, alpha=0.5)
 	plt.axhline(0, alpha=0.5)
 
 	plt.show()
+
 	return None
-
-
-
-
-
