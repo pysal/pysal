7436f38bae9fcb2756a7f5b6d8785a6098acbf59
Taylor Oshan <tayoshan@gmail.com>
2016-06-01 18:29:15 -0700

    add custom glm

--- pysal/contrib/glm/iwls.py
+++ pysal/contrib/glm/iwls.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.linalg as la
-import families
+from family import Binomial
 
 
 def _compute_betas(y, x):
@@ -34,48 +34,46 @@ def _compute_betas_gwr(y, x, wi):
     return betas, xtx_inv_xt
 
 def iwls(y, x, family, offset, y_fix,
-    ini_betas=None, tol=1.0e-6, max_iter=200, wi=None):
+    ini_betas=None, tol=1.0e-8, max_iter=200, wi=None):
     """
     Iteratively re-weighted least squares estimation routine
 
     """
-    diff = 1.0e6
     n_iter = 0
+    diff = 1.0e6
     link_y = family.link(y)
-
-    if isinstance(family, families.Binomial):
-        link_y = family.link._clean(link_y)
+    if isinstance(family, Binomial):
+        y = family.link._clean(y)
     if ini_betas is None:
-        betas = _compute_betas(link_y, x)
-    else:
-        betas = ini_betas
-    v = np.dot(x, betas)
+        #betas = _compute_betas(link_y, x)
+        betas = np.zeros((x.shape[1], 1), np.float)
+        mu = family.starting_mu(y)
+        v = family.predict(mu)
+    #else:
+        #betas = ini_betas
+    #v = np.dot(x, betas)
    
     while diff > tol and n_iter < max_iter:
-        n_iter += 1
-       
-        mu = family.link.inverse(v)
+    	n_iter += 1
+        #mu = family.link.inverse(v)
         w = family.weights(mu)
         z = v + (family.link.deriv(mu)*(y-mu))
         w = np.sqrt(w)
         wx = x * w
         wz = z * w
+        
         if wi is None:
             n_betas = _compute_betas(wz, wx)
         else:
             n_betas, xtx_inv_xt = _compute_betas_gwr(wz, wx, wi)
-        v_new = np.dot(x, n_betas)
-	    #if family == 'Gaussian':
-            #diff = 0.0
-	        #v = v_new
-	    #else
         
-        diff = min(abs(n_betas-betas))
-        v = v_new
-        betas = n_betas
-    
-    y_hat = family.fitted(v)
+        v = np.dot(x, n_betas)
+        mu  = family.fitted(v)
 
+        diff = min(abs(n_betas-betas))
+        betas = n_betas
+        
+    y_hat = mu
     if wi is None:
         return betas, y_hat, w, n_iter
     else:
