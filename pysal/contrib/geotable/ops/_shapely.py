9e99b6ffe45a57d3db59fa1959e0d82663e0d6ee
ljwolf <levi.john.wolf@gmail.com>
2016-06-25 23:12:56 -0700

    enforce dependency-safety around contrib module

--- pysal/contrib/geotable/ops/_shapely.py
+++ pysal/contrib/geotable/ops/_shapely.py
@@ -1,5 +1,6 @@
 import functools as _f
 from warnings import warn 
+from ....common import requires as _requires
 
 __all__ = ["to_wkb", "to_wkt", "area", "distance", "length", "boundary", "bounds", "centroid", "representative_point", "convex_hull", "envelope", "buffer", "simplify", "difference", "intersection", "symmetric_difference", "union", "has_z", "is_empty", "is_ring", "is_simple", "is_valid", "relate", "contains", "crosses", "disjoint", "equals", "intersects", "overlaps", "touches", "within", "equals_exact", "almost_equals", "project", "interpolate"]
 
@@ -42,9 +43,9 @@ pysal.contrib.shapely_ext.{n}
 """
 
 # ensure that the construction of atomics is done only if we can use shapely
+_shapely_atomics = {}
 try:
     from ... import shapely_ext as _s
-    _shapely_atomics = {}
     for k in __all__:
         _shapely_atomics.update({k:_f.partial(_atomic_op, _func=_s.__dict__[k])})
         _shapely_atomics[k].__doc__ = _doc_template.format(n=_s.__dict__[k].__name__)
@@ -57,6 +58,7 @@ globals().update(_shapely_atomics)
 # Reductions #
 ##############
 
+@_requires('shapely')
 def cascaded_union(df, geom_col='geometry', **groupby_kws):
     """
     Returns the cascaded union of a possibly-grouped dataframe
@@ -87,6 +89,7 @@ def cascaded_union(df, geom_col='geometry', **groupby_kws):
     out = df.geometry.apply(_s.cascaded_union)
     return out
 
+@_requires('shapely')
 def unary_union(df, geom_col='geometry', **groupby_kws):
     """
     Returns the cascaded union of a possibly-grouped dataframe
@@ -117,6 +120,7 @@ def unary_union(df, geom_col='geometry', **groupby_kws):
     out = df.geometry.apply(_s.cascaded_union)
     return out
 
+@_requires('shapely')
 def _cascaded_intersection(shapes):
     it = iter(shapes)
     outshape = next(it)
@@ -128,6 +132,7 @@ def _cascaded_intersection(shapes):
             return None
     return outshape
 
+@_requires('shapely')
 def cascaded_intersection(df, geom_col='geometry', **groupby_kws): 
     """
     Returns the cascaded union of a possibly-grouped dataframe
