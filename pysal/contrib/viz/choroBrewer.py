5c30cb0929e2094c6781ee448ed0283193e3bcb5
Serge Rey <sjsrey@gmail.com>
2016-07-18 15:01:30 -0700

    Cmap display option, fig_size option

--- pysal/contrib/viz/choroBrewer.py
+++ pysal/contrib/viz/choroBrewer.py
@@ -1,17 +1,14 @@
 
 
 get_ipython().magic(u'matplotlib inline')
-from ipywidgets import Dropdown, RadioButtons, IntSlider, VBox, FloatSlider, Button, interact
-from ipywidgets import HBox
+from ipywidgets import Dropdown, RadioButtons,  HBox, interact
 from IPython.display import display
 from pysal.contrib.viz.color import color_display_types
 from pysal.contrib.viz.color import plot_cmaps as pcmaps
 from pysal.contrib.viz import mapping as maps
-import pysal as ps
 
 
-
-def choro(gd, col):
+def choro(gd, col, show_cmaps=True, fig_size=(6, 6)):
     dt = sorted(color_display_types.keys(), reverse=True)
     data_type = RadioButtons(description='Data Type', options=dt)
     bindings = {}
@@ -24,7 +21,6 @@ def choro(gd, col):
         with cmap_dd.hold_trait_notifications():
             cmap_dd.options = bindings[change['new']]
             k_dd.options = kbindings[change['new']]
-            #plot_cmaps('diverging')
 
     def cmap_change(change):
             with cmap_dd.hold_trait_notifications():
@@ -32,16 +28,15 @@ def choro(gd, col):
 
     data_type.observe(type_change, names=['value'])
     cmap_dd.observe(cmap_change, names=['value'])
-    kbindings = {'sequential': range(3,9+1),
-                'qualitative': range(3,12+1),
-                'diverging': range(3,11+1)}
+    kbindings = {'sequential': range(3, 9+1), 'qualitative': range(3, 12+1),
+                 'diverging': range(3, 11+1)}
     k_dd = Dropdown(description='k', options=kbindings[data_type.value])
-
     display(HBox([data_type, k_dd]))
+
     @interact(cmap=cmap_dd)
     def plot_cmaps(cmap):
         i = cmap_dd.options.index(cmap)
-        pcmaps(data_type.value, i)
-        maps.geoplot(gd, col, k=int(k_dd.value),
-                    palette=cmap_dd.value, dtype=data_type.value,
-                    figsize=(6, 6))
+        if show_cmaps:
+            pcmaps(data_type.value, i)
+        maps.geoplot(gd, col, k=int(k_dd.value), palette=cmap_dd.value,
+                     dtype=data_type.value, figsize=fig_size)
