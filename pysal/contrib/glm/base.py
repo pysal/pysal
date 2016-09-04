fa0996ca414d03d510bb78a76fe80427cae9abba
Taylor Oshan <tayoshan@gmail.com>
2016-09-04 08:44:09 -0700

    remove predict function

--- pysal/contrib/glm/base.py
+++ pysal/contrib/glm/base.py
@@ -25,43 +25,6 @@ class Results(object):
         if hasattr(model, 'k_constant'):
             self.k_constant = model.k_constant
 
-    def predict(self, exog=None, transform=True, *args, **kwargs):
-        """
-        Call self.model.predict with self.params as the first argument.
-        Parameters
-        ----------
-        exog : array-like, optional
-            The values for which you want to predict.
-        transform : bool, optional
-            If the model was fit via a formula, do you want to pass
-            exog through the formula. Default is True. E.g., if you fit
-            a model y ~ log(x1) + log(x2), and transform is True, then
-            you can pass a data structure that contains x1 and x2 in
-            their original form. Otherwise, you'd need to log the data
-            first.
-        args, kwargs :
-            Some models can take additional arguments or keywords, see the
-            predict method of the model for the details.
-        Returns
-        -------
-        prediction : ndarray or pandas.Series
-            See self.model.predict
-        """
-        if transform and hasattr(self.model, 'formula') and exog is not None:
-            from patsy import dmatrix
-            exog = dmatrix(self.model.data.design_info.builder,
-                           exog)
-
-        if exog is not None:
-            exog = np.asarray(exog)
-            if exog.ndim == 1 and (self.model.exog.ndim == 1 or
-                                   self.model.exog.shape[1] == 1):
-                exog = exog[:, None]
-            exog = np.atleast_2d(exog)  # needed in count model shape[1]
-
-        return self.model.predict(self.params, exog, *args, **kwargs)
-
-
 #TODO: public method?
 class LikelihoodModelResults(Results):
     """
