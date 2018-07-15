26988982cda8a624897ad915b7481838a7126e4a
Serge Rey <sjsrey@gmail.com>
2018-07-14 22:33:15 -0500

    start on splot from master

--- convert.py
+++ convert.py
@@ -22,7 +22,10 @@ c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/libpysal/pysal\.lib/g
 os.system(c)
 
 # replace all references to esda with pysal.explore.esda
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
+c = "find pysal/explore/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
+os.system(c)
+
+c = "find pysal/dynamics/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
 os.system(c)
 
 # replace all references to mapclassify with pysal.viz.mapclassify
@@ -93,10 +96,85 @@ os.system(c)
 
 # fix spvcm
 
-c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/ps\.examples/pysal\.lib\.examples/g'"
+c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
+os.system(c)
+
+#pysal.queen_from_shapefile
+
+c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.queen/pysal\.lib\.weights\.user\.queen/g'"
 os.system(c)
 
 
+
+c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.w_subset/pysal\.lib\.weights\.Wsets\.w_subset/g'"
+os.system(c)
+
+# fix splot
+#from splot.libpysal import plot_spatial_weights
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.libpysal/from pysal\.viz\.splot\.libpysal/g'"
+os.system(c)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.bk/from pysal\.viz\.splot\.bk/g'"
+os.system(c)
+
+#from esda.moran import Moran_Local, Moran, Moran_BV, Moran_Local_BV
+
+#c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
+#os.system(c)
+
+# from pysal.viz.splot.pysal.explore.esda import 
+# from splot.esda
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from pysal\.viz\.splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.esda/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.pysal\.explore\.esda/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal as ps/import pysal/g'"
+os.system(c)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.spreg/pysal\.model\.spreg/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.lag_spatial/pysal\.lib\.weights\.spatial_lag\.lag_spatial/g'"
+os.system(c)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import esda/import pysal\.explore\.esda/g'"
+os.system(c)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal\.explore\.esda/import pysal\.explore\.esda as esda/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.esda/from pysal\.viz\.splot\.esda/g'"
+os.system(c)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
+os.system(c)
+
+#from splot.libpysal import plot_spatial_weights
+#from splot.pysal.lib import plot_spatial_weights
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.lib/from pysal\.viz\.splot\.libpysal/g'"
+os.system(c)
+
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.giddy/from pysal\.viz\.splot\.giddy/g'"
+os.system(c)
+
+
+#from ._viz_pysal.lib_mpl import (plot_spatial_weights)
+#from ._viz_libpysal_mpl import (plot_spatial_weights)
+
+c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/_viz_pysal\.lib_mpl/_viz_libpysal_mpl/g'"
+os.system(c)
 #
 #
 #c = "find pysal/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.examples\.open/g'"
