25c32fa01401974ad72a54c4de00219b9dfd8693
Serge Rey <sjsrey@gmail.com>
2018-07-17 15:16:13 -0700

    renaming

--- convert.py
+++ convert.py
@@ -3,9 +3,9 @@ import os
 import subprocess
 import sys
 
-TARGETROOT ="pysal/"
+TARGETROOT ="pysalnext/"
 
-os.system('rm pysal/*.py')
+os.system('rm pysalnext/*.py')
 
 with open('packages.yml') as package_file:
     packages = yaml.load(package_file)
@@ -13,214 +13,216 @@ with open('packages.yml') as package_file:
 
 
 for package in packages:
-    com = "rm -fr pysal/{package}".format(package=package)
+    com = "rm -fr pysalnext/{package}".format(package=package)
     os.system(com)
-    com = "mkdir pysal/{package}".format(package=package)
+    com = "mkdir pysalnext/{package}".format(package=package)
     os.system(com)
 
     #"cp -fr tmp/{subpackage}-master/{subpackage}/* pysal/{package}/".format(package=package, subpackage=subpackage)
     subpackages = packages[package].split()
     for subpackage in subpackages:
         if subpackage == 'libpysal':
-            com = "cp -rf tmp/{subpackage}/{subpackage}/*  pysal/{package}/".format(package=package, subpackage=subpackage)
+            com = "cp -rf tmp/{subpackage}/{subpackage}/*  pysalnext/{package}/".format(package=package, subpackage=subpackage)
         else:
-            com = "cp -rf tmp/{subpackage}/{subpackage} pysal/{package}/{subpackage}".format(package=package, subpackage=subpackage)
+            com = "cp -rf tmp/{subpackage}/{subpackage} pysalnext/{package}/{subpackage}".format(package=package, subpackage=subpackage)
         print(com)
         os.system(com)
 
 # replace all references to libpysal with pysal.lib
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/libpysal/pysal\.lib/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/libpysal/pysal\.lib/g'"
 os.system(c)
 
 # replace all references to esda with pysal.explore.esda
-c = "find pysal/explore/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
+c = "find pysalnext/explore/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
 os.system(c)
 
-c = "find pysal/dynamics/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
+c = "find pysalnext/dynamics/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
 os.system(c)
 
 # replace all references to mapclassify with pysal.viz.mapclassify
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/mapclassify/pysal\.viz\.mapclassify/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/mapclassify/pysal\.viz\.mapclassify/g'"
 os.system(c)
 
 # replace all references to pysal.spreg with pysal.model.spreg
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/pysal\.spreg/pysal\.model\.spreg/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/pysal\.spreg/pysal\.model\.spreg/g'"
 os.system(c)
 
 # replace all references in spglm to spreg with pysal.model.spreg
-c = "find pysal/model/spglm/. -name '*.py' -print | xargs sed -i -- 's/ spreg\./ pysal\.model\.spreg\./g'"
+c = "find pysalnext/model/spglm/. -name '*.py' -print | xargs sed -i -- 's/ spreg\./ pysal\.model\.spreg\./g'"
 os.system(c)
 
 # replace all references in spint to spreg with pysal.model.spreg
-c = "find pysal/model/spint/. -name '*.py' -print | xargs sed -i -- 's/from spreg import/from pysal\.model\.spreg import/g'"
+c = "find pysalnext/model/spint/. -name '*.py' -print | xargs sed -i -- 's/from spreg import/from pysal\.model\.spreg import/g'"
 os.system(c)
 
 
 # replace all references in spint to spreg with pysal.model.spreg
-c = "find pysal/model/spint/. -name '*.py' -print | xargs sed -i -- 's/ spreg\./ pysal\.model\.spreg\./g'"
+c = "find pysalnext/model/spint/. -name '*.py' -print | xargs sed -i -- 's/ spreg\./ pysal\.model\.spreg\./g'"
 os.system(c)
 
 
 # replace all references to spglm with pysal.model.spglm
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/spglm/pysal\.model\.spglm/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/spglm/pysal\.model\.spglm/g'"
 os.system(c)
 
 
 # replace all references to spvcm with pysal.model.spvcm
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ spvcm / pysal\.model\.spvcm /g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/ spvcm / pysal\.model\.spvcm /g'"
 os.system(c)
 
 # replace all references to spvcm with pysal.model.spvcm
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ spvcm\./ pysal\.model\.spvcm\./g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/ spvcm\./ pysal\.model\.spvcm\./g'"
 os.system(c)
 
 
 # fix giddy
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ giddy\.api/ pysal\.dynamics\.giddy\.api/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/ giddy\.api/ pysal\.dynamics\.giddy\.api/g'"
 os.system(c)
 
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/import giddy/import pysal\.dynamics\.giddy/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/import giddy/import pysal\.dynamics\.giddy/g'"
 os.system(c)
 
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/from giddy/from pysal\.dynamics\.giddy/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/from giddy/from pysal\.dynamics\.giddy/g'"
 os.system(c)
 
 # fix gwr
-c = "find pysal/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.open/g'"
+c = "find pysalnext/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.open/g'"
 os.system(c)
 
 
-c = "find pysal/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
+c = "find pysalnext/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
 os.system(c)
 
 
 # fix spreg
 
-c = "find pysal/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
+c = "find pysalnext/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
 os.system(c)
 
-c = "find pysal/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/import spreg/import pysal\.model\.spreg/g'"
+c = "find pysalnext/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/import spreg/import pysal\.model\.spreg/g'"
 os.system(c)
 
-c = "find pysal/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/ spreg/ pysal\.model\.spreg/g'"
+c = "find pysalnext/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/ spreg/ pysal\.model\.spreg/g'"
 os.system(c)
 
 # fix spvcm
 
-c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
+c = "find pysalnext/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
 os.system(c)
 
 #pysal.queen_from_shapefile
 
-c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.queen/pysal\.lib\.weights\.user\.queen/g'"
+c = "find pysalnext/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.queen/pysal\.lib\.weights\.user\.queen/g'"
 os.system(c)
 
 
 
-c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.w_subset/pysal\.lib\.weights\.Wsets\.w_subset/g'"
+c = "find pysalnext/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.w_subset/pysal\.lib\.weights\.Wsets\.w_subset/g'"
 os.system(c)
 
 # fix splot
 #from splot.libpysal import plot_spatial_weights
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.libpysal/from pysal\.viz\.splot\.libpysal/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.libpysal/from pysal\.viz\.splot\.libpysal/g'"
 os.system(c)
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.bk/from pysal\.viz\.splot\.bk/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.bk/from pysal\.viz\.splot\.bk/g'"
 os.system(c)
 
 #from esda.moran import Moran_Local, Moran, Moran_BV, Moran_Local_BV
 
-#c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
+#c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
 #os.system(c)
 
 # from pysal.viz.splot.pysal.explore.esda import 
 # from splot.esda
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from pysal\.viz\.splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from pysal\.viz\.splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.esda/g'"
 os.system(c)
 
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.pysal\.explore\.esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.pysal\.explore\.esda/g'"
 os.system(c)
 
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal as ps/import pysal/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal as ps/import pysal/g'"
 os.system(c)
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.spreg/pysal\.model\.spreg/g'"
-os.system(c)
-
-
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.lag_spatial/pysal\.lib\.weights\.spatial_lag\.lag_spatial/g'"
-os.system(c)
-
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.spreg/pysal\.model\.spreg/g'"
 os.system(c)
 
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import esda/import pysal\.explore\.esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.lag_spatial/pysal\.lib\.weights\.spatial_lag\.lag_spatial/g'"
 os.system(c)
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal\.explore\.esda/import pysal\.explore\.esda as esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
 os.system(c)
 
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.esda/from pysal\.viz\.splot\.esda/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import esda/import pysal\.explore\.esda/g'"
 os.system(c)
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal\.explore\.esda/import pysal\.explore\.esda as esda/g'"
+os.system(c)
+
+
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.esda/from pysal\.viz\.splot\.esda/g'"
+os.system(c)
+
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
 os.system(c)
 
 #from splot.libpysal import plot_spatial_weights
 #from splot.pysal.lib import plot_spatial_weights
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.lib/from pysal\.viz\.splot\.libpysal/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.lib/from pysal\.viz\.splot\.libpysal/g'"
 os.system(c)
 
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.giddy/from pysal\.viz\.splot\.giddy/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.giddy/from pysal\.viz\.splot\.giddy/g'"
 os.system(c)
 
 
 #from ._viz_pysal.lib_mpl import (plot_spatial_weights)
 #from ._viz_libpysal_mpl import (plot_spatial_weights)
 
-c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/_viz_pysal\.lib_mpl/_viz_libpysal_mpl/g'"
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/_viz_pysal\.lib_mpl/_viz_libpysal_mpl/g'"
 os.system(c)
 #
 #
-#c = "find pysal/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.examples\.open/g'"
+#c = "find pysalnext/model/gwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.examples\.open/g'"
 #os.system(c)
 
 
 # final change to pysalnext from pysal
 
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/pysal/pysalnext/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/pysal/pysalnext/g'"
 os.system(c)
 
+c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/\.libpysalnext//g'"
+os.system(c)
 
 
 """
 
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/\.vizpysal\./\./g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/\.vizpysal\./\./g'"
 os.system(c)
 
 
 
 # replace all references to .spint with pysal.model.spint
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/spint\./pysal\.model\.spint\./g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/spint\./pysal\.model\.spint\./g'"
 os.system(c)
 
 # fix libpysal.api now that it has just been clobbered
-c = "find pysal/. -name 'api.py' -print | xargs sed -i -- 's/weights\.pysal\.model\.spint/weights\.spintW/g'"
+c = "find pysalnext/. -name 'api.py' -print | xargs sed -i -- 's/weights\.pysal\.model\.spint/weights\.spintW/g'"
 os.system(c)
 
 # replace all references to gwr with pysal.model.gwr
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ gwr / pysal\.model\.gwr/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/ gwr / pysal\.model\.gwr/g'"
 os.system(c)
 
 
 # replace all references to .legendgram with pysal.viz.legendgram
-c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/\.legendgram/pysal\.viz\.legendgram/g'"
+c = "find pysalnext/. -name '*.py' -print | xargs sed -i -- 's/\.legendgram/pysal\.viz\.legendgram/g'"
 os.system(c)
 
 
@@ -259,5 +261,5 @@ for package in packages:
 
 lines = "\n".join(init_lines)
 
-with open("pysal/__init__.py", 'w') as outfile:
+with open("pysalnext/__init__.py", 'w') as outfile:
     outfile.write(lines)
--- download.py
+++ download.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 import sys
 
-TARGETROOT ="pysal/"
 
 
 with open('packages.yml') as package_file:
@@ -14,31 +13,6 @@ for package in packages:
     #print(package)
     subpackages = packages[package].split()
     for subpackage in subpackages:
-        """
-        #print(package, subpackage)
-        #print(upstream)
-
-        # get the zip of the current master branch (or release?) for the subpackage
-        pipcom = 'pip download '+subpackage+' --no-binary :all: --no-deps -d tmp'
-        tarcom = 'tar xzf tmp/'+subpackage+"*.tar.gz -C tmp; rm tmp/*.gz"
-
-        zipcom = 'unzip tmp/'+subpackage+"*.zip -d tmp"
-        cpcom = 'cp -fr tmp/'+subpackage+"*/"+subpackage+" "+"pysal/"+package+"/"
-        #print(pipcom)
-        os.system(pipcom)
-        #print(tarcom)
-        tar_res = os.system(tarcom)
-        if tar_res != 0:
-            zip_res = os.system(zipcom)
-            if zip_res != 0:
-                print('Neither a zip or tarball is available for %s'% subpackage)
-        #print(mvcom)
-        os.system(cpcom)
-
-        # pip download subpackage -d tmp --no-deps
-        # tar xzf tmp/subpackage*.gz -C tmp
-        # mv  tmp/subpackage*/subpackage subparent/
-        """
         pkgstr = "https://github.com/pysal/%s/archive/master.zip" % subpackage 
         downloadcommand = "wget %s; mv master.zip tmp/." % pkgstr
         #downloadcommand = "wget https://github.com/pysal/splot/archive/master.zip; mv master.zip tmp/."
