1961034a8dc38b2c29c60e637cb10eb93e00b2c9
ljwolf <levi.john.wolf@gmail.com>
2018-07-17 11:47:04 -0700

    chore: adding blank inits for all modules

--- .travis_testing.sh
+++
@@ -1,5 +0,0 @@
-if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
-    nosetests --with-coverage --cover-package=pysal --exclude-dir=pysal/contrib
-else
-    nosetests --with-coverage --cover-package=pysal; 
-fi
