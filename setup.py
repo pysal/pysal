ae0b4d32c59b5882c2bf5cf3bcfabbaaf2dff411
Serge Rey <sjsrey@gmail.com>
2019-05-03 12:43:37 -0700

    enh: travis matrix specification

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ def setup_package():
             'Topic :: Scientific/Engineering :: GIS',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7'
         ],
         package_data={'pysal': list(example_data_files)},
         install_requires=install_reqs,
