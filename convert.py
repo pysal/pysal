189f2fd29a2a979a8bead22f9d0593123300b5af
Serge Rey <sjsrey@gmail.com>
2019-06-15 18:02:43 -0700

    ENH: adding segregation

--- convert.py
+++ convert.py
@@ -4,16 +4,15 @@ import subprocess
 import sys
 import json
 
-#TARGETROOT ="pysal/"
+# TARGETROOT ="pysal/"
 
-os.system('rm pysal/*.py')
+os.system("rm pysal/*.py")
 
-with open('packages.yml') as package_file:
+with open("packages.yml") as package_file:
     packages = yaml.load(package_file)
 
-
 # only tagged packages go in release
-with open('tags.json') as tag_file:
+with open("tags.json") as tag_file:
     tags = json.load(tag_file)
 
 tagged = list(tags.keys())
@@ -26,18 +25,22 @@ for package in packages:
     com = "mkdir pysal/{package}".format(package=package)
     os.system(com)
 
-    #"cp -fr tmp/{subpackage}-master/{subpackage}/* pysal/{package}/".format(package=package, subpackage=subpackage)
+    # "cp -fr tmp/{subpackage}-master/{subpackage}/* pysal/{package}/".format(package=package, subpackage=subpackage)
     subpackages = packages[package].split()
     for subpackage in subpackages:
-        if subpackage == 'libpysal':
-            com = "cp -rf tmp/{subpackage}/{subpackage}/*  pysal/{package}/".format(package=package, subpackage=subpackage)
+        if subpackage == "libpysal":
+            com = "cp -rf tmp/{subpackage}/{subpackage}/*  pysal/{package}/".format(
+                package=package, subpackage=subpackage
+            )
         else:
-            com = "cp -rf tmp/{subpackage}/{subpackage} pysal/{package}/{subpackage}".format(package=package, subpackage=subpackage)
+            com = "cp -rf tmp/{subpackage}/{subpackage} pysal/{package}/{subpackage}".format(
+                package=package, subpackage=subpackage
+            )
         if subpackage in tagged:
             print(com)
             os.system(com)
         else:
-            print('skipping: ', subpackage)
+            print("skipping: ", subpackage)
 
 # replace all references to libpysal with pysal.lib
 c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/libpysal/pysal\.lib/g'"
@@ -46,7 +49,6 @@ os.system(c)
 # replace all references to esda with pysal.explore.esda
 c = "find pysal/explore/. -name '*.py' -print | xargs sed -i -- 's/esda/pysal\.explore\.esda/g'"
 os.system(c)
-
 
 # replace all references to mapclassify in esda and viz.mappclassify with pysal.viz.mapclassify
 c = "find pysal/viz/mapclassify/. -name '*.py' -print | xargs sed -i -- 's/mapclassify/pysal\.viz\.mapclassify/g'"
@@ -70,14 +72,13 @@ os.system(c)
 c = "find pysal/model/spint/. -name '*.py' -print | xargs sed -i -- 's/from spreg import/from pysal\.model\.spreg import/g'"
 os.system(c)
 
-
 # replace all references in spint to spreg with pysal.model.spreg
 c = "find pysal/model/spint/. -name '*.py' -print | xargs sed -i -- 's/ spreg\./ pysal\.model\.spreg\./g'"
 os.system(c)
 
 # replace import spreg.user_output as USER    to     from pysal.model.spreg import user_output as USER
-#c = "find pysal/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/import spreg\.user_output as USER/from pysal\.model\.spreg import user_output as USER/g'"
-#os.system(c)
+# c = "find pysal/model/spreg/. -name '*.py' -print | xargs sed -i -- 's/import spreg\.user_output as USER/from pysal\.model\.spreg import user_output as USER/g'"
+# os.system(c)
 
 c = "find pysal/model/spglm/. -name '*.py' -print | xargs sed -i -- 's/import spreg\.user_output as USER/from pysal\.model\.spreg import user_output as USER/g'"
 os.system(c)
@@ -98,7 +99,6 @@ os.system(c)
 c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/spglm/pysal\.model\.spglm/g'"
 os.system(c)
 
-
 # replace all references to spvcm with pysal.model.spvcm
 c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ spvcm / pysal\.model\.spvcm /g'"
 os.system(c)
@@ -111,7 +111,6 @@ os.system(c)
 
 c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
 os.system(c)
-
 
 # fix giddy
 c = "find pysal/. -name '*.py' -print | xargs sed -i -- 's/ giddy\.api/ pysal\.explore\.giddy\.api/g'"
@@ -127,10 +126,8 @@ os.system(c)
 c = "find pysal/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.open/g'"
 os.system(c)
 
-
 c = "find pysal/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
 os.system(c)
-
 
 # fix spreg
 
@@ -148,39 +145,36 @@ os.system(c)
 c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.examples/pysal\.lib\.examples/g'"
 os.system(c)
 
-#pysal.queen_from_shapefile
+# pysal.queen_from_shapefile
 
 c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.queen/pysal\.lib\.weights\.user\.queen/g'"
 os.system(c)
 
-
 c = "find pysal/model/spvcm/. -name '*.py' -print | xargs sed -i -- 's/pysal\.w_subset/pysal\.lib\.weights\.Wsets\.w_subset/g'"
 os.system(c)
 
-if 'splot' in tagged:
+if "splot" in tagged:
 
     # fix splot
-    #from splot.libpysal import plot_spatial_weights
+    # from splot.libpysal import plot_spatial_weights
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.libpysal/from pysal\.viz\.splot\.libpysal/g'"
     os.system(c)
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.bk/from pysal\.viz\.splot\.bk/g'"
     os.system(c)
 
-    #from esda.moran import Moran_Local, Moran, Moran_BV, Moran_Local_BV
+    # from esda.moran import Moran_Local, Moran, Moran_BV, Moran_Local_BV
 
-    #c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
-    #os.system(c)
+    # c = "find pysalnext/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
+    # os.system(c)
 
-    # from pysal.viz.splot.pysal.explore.esda import 
+    # from pysal.viz.splot.pysal.explore.esda import
     # from splot.esda
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from pysal\.viz\.splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.esda/g'"
     os.system(c)
 
-
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.explore\.esda/from pysal\.viz\.splot\.pysal\.explore\.esda/g'"
     os.system(c)
-
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal as ps/import pysal/g'"
     os.system(c)
@@ -188,13 +182,11 @@ if 'splot' in tagged:
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.spreg/pysal\.model\.spreg/g'"
     os.system(c)
 
-
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/ps\.lag_spatial/pysal\.lib\.weights\.spatial_lag\.lag_spatial/g'"
     os.system(c)
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from esda/from pysal\.explore\.esda/g'"
     os.system(c)
-
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import esda/import pysal\.explore\.esda/g'"
     os.system(c)
@@ -202,26 +194,23 @@ if 'splot' in tagged:
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import pysal\.explore\.esda/import pysal\.explore\.esda as esda/g'"
     os.system(c)
 
-
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.esda/from pysal\.viz\.splot\.esda/g'"
     os.system(c)
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
     os.system(c)
 
-    #from splot.libpysal import plot_spatial_weights
-    #from splot.pysal.lib import plot_spatial_weights
+    # from splot.libpysal import plot_spatial_weights
+    # from splot.pysal.lib import plot_spatial_weights
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.pysal\.lib/from pysal\.viz\.splot\.libpysal/g'"
     os.system(c)
 
-
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.giddy/from pysal\.viz\.splot\.giddy/g'"
     os.system(c)
 
-
-    #from ._viz_pysal.lib_mpl import (plot_spatial_weights)
-    #from ._viz_libpysal_mpl import (plot_spatial_weights)
+    # from ._viz_pysal.lib_mpl import (plot_spatial_weights)
+    # from ._viz_libpysal_mpl import (plot_spatial_weights)
 
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/_viz_pysal\.lib_mpl/_viz_libpysal_mpl/g'"
     os.system(c)
@@ -230,20 +219,18 @@ if 'splot' in tagged:
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/import mapclassify/import pysal\.viz\.mapclassify/g'"
     os.system(c)
 
-    # from splot.mapping -> from pysal.viz.splot.mapping 
+    # from splot.mapping -> from pysal.viz.splot.mapping
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\.mapping /from pysal\.viz\.splot\.mapping /g'"
     os.system(c)
 
-    # from splot._ -> from pysal.viz.splot._  
+    # from splot._ -> from pysal.viz.splot._
     c = "find pysal/viz/splot/. -name '*.py' -print | xargs sed -i -- 's/from splot\._/from pysal\.viz\.splot\._/g'"
     os.system(c)
 
-
 #
 #
-#c = "find pysalnext/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.examples\.open/g'"
-#os.system(c)
-
+# c = "find pysalnext/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/pysal\.open/pysal\.lib\.examples\.open/g'"
+# os.system(c)
 
 # handle inequality
 
@@ -252,36 +239,45 @@ os.system(c)
 
 # handle mgwr
 
-
 c = "find pysal/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/from spreg/from pysal\.model\.spreg/g'"
 os.system(c)
 
 c = "find pysal/model/mgwr/. -name '*.py' -print | xargs sed -i -- 's/import spreg/import pysal\.model\.spreg/g'"
 os.system(c)
 
-
 # handle spaghetti
 
 c = "find pysal/explore/spaghetti/. -name '*.py' -print | xargs sed -i -- 's/from spaghetti/from pysal\.explore\.spaghetti/g'"
 os.system(c)
 
-
 c = "find pysal/explore/spaghetti/. -name '*.py' -print | xargs sed -i -- 's/import spaghetti/import pysal\.explore\.spaghetti/g'"
+os.system(c)
+
+# handle segregation
+
+c = "find pysal/explore/segregation/. -name '*.py' -print | xargs sed -i -- 's/from segregation/from pysal\.explore\.segregation/g'"
+os.system(c)
+
+c = "find pysal/explore/segregation/. -name '*.py' -print | xargs sed -i -- 's/import segregation/import pysal\.explore\.segregation/g'"
+os.system(c)
+
+c = "find pysal/explore/segregation/. -name '*.py' -print | xargs sed -i -- 's/w_pysal\.lib/w_libpysal/g'"
+
 os.system(c)
 
 init_lines = ["__version__='2.1.0dev'"]
 for package in packages:
-    os.system('touch pysal/{package}/__init__.py'.format(package=package)) 
+    os.system("touch pysal/{package}/__init__.py".format(package=package))
     subpackages = packages[package].split()
-    if package == 'lib':
+    if package == "lib":
         pass
     else:
-        subpackage_lines = ['from . import {}'.format(s) for s in subpackages]
-        with open('pysal/{package}/__init__.py'.format(package=package), 'w') as f:
-            f.write('\n'.join(subpackage_lines))
-    init_lines.append('from . import {}'.format(package))
+        subpackage_lines = ["from . import {}".format(s) for s in subpackages]
+        with open("pysal/{package}/__init__.py".format(package=package), "w") as f:
+            f.write("\n".join(subpackage_lines))
+    init_lines.append("from . import {}".format(package))
 
 lines = "\n".join(init_lines)
 
-with open("pysal/__init__.py", 'w') as outfile:
+with open("pysal/__init__.py", "w") as outfile:
     outfile.write(lines)
