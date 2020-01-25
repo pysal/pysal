cfe1dfbaa233555fe58a33892e0f87ea9b6965ac
Serge Rey <sjsrey@gmail.com>
2020-01-24 18:58:49 -0800

    ENH: tooling for checking versions of subpackages

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -3,3 +3,20 @@ from . import lib
 from . import explore
 from . import viz
 from . import model
+
+
+def check_versions():
+    from .base import frozen_packages
+    import warnings
+    current_packages = {}
+    for pkg in frozen_packages.keys():
+        exec(f'import {pkg}')
+        current_packages[pkg] = eval(f'{pkg}.__version__')
+    for pkg in frozen_packages.keys():
+        if frozen_packages[pkg] != current_packages[pkg]:
+            msg = (
+                f"The last meta package for PySAL included {pkg} "
+                f"at version {frozen_packages[pkg]}, but you have "
+                f"a more recent version {current_packages[pkg]}."
+                )
+            warnings.warn(msg)
---
+++ pysal/base.py
@@ -0,0 +1,20 @@
+"""
+Frozen subpackages for meta release.
+"""
+
+frozen_packages = {	"libpysal": "4.2.1",
+	"esda": "2.2.0",
+	"giddy": "2.3.0",
+	"inequality": "1.0.0",
+	"pointpats": "2.1.0",
+	"segregation": "1.1.1",
+	"spaghetti": "1.4.0",
+	"mgwr": "2.1.1",
+	"spglm": "1.0.7",
+	"spint": "1.0.6",
+	"spreg": "1.0.4",
+	"spvcm": "0.2.1.post1",
+	"tobler": "0.2.0",
+	"mapclassify": "2.2.0",
+	"splot": "1.1.2"
+}
