4072bacfd128b1fa488c2d860a485116d4c68d2c
Serge Rey <sjsrey@gmail.com>
2020-01-25 07:39:25 -0800

    ENH: update message for version checks on subpackages

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -8,15 +8,20 @@ from . import model
 def check_versions():
     from .base import frozen_packages
     import warnings
+    frozen_packages.pop('spvcm')
     current_packages = {}
     for pkg in frozen_packages.keys():
         exec(f'import {pkg}')
         current_packages[pkg] = eval(f'{pkg}.__version__')
+    diffs = 0
     for pkg in frozen_packages.keys():
         if frozen_packages[pkg] != current_packages[pkg]:
+            diffs += 1
             msg = (
                 f"The last meta package for PySAL included {pkg} "
                 f"at version {frozen_packages[pkg]}, but you have "
-                f"a more recent version {current_packages[pkg]}."
+                f"version {current_packages[pkg]}."
                 )
             warnings.warn(msg)
+    if diffs == 0:
+        print(f'PySAL subpackages all pinned to meta release {__version__}.')
