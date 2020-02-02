1d67c66597a7ca256b47c2db7563ee9c1cc949f3
Serge Rey <sjsrey@gmail.com>
2020-02-02 09:42:57 -0800

    ENH: utilities for checking versions of packages in the meta release

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -4,44 +4,5 @@ from . import explore
 from . import viz
 from . import model
 
-from .base import memberships, federation_hierarchy
+from .base import memberships, federation_hierarchy, versions
 
-
-def installed_version(package):
-    exec(f'import {package}')
-    try:
-        v = eval(f'{package}.__version__')  ## FIX when spvcm is 0.3.0
-    except:
-        v = None
-    return v
-
-def installed_versions():
-    ver = {}
-    for package in memberships.keys():
-        ver[package] = installed_version(package)
-    return ver
-
-def released_versions():
-    from .frozen import frozen_packages
-    return frozen_packages
-
-def check_versions():
-    from .frozen import frozen_packages 
-    import warnings
-    frozen_packages.pop('spvcm')
-    current_packages = {}
-    for pkg in frozen_packages.keys():
-        exec(f'import {pkg}')
-        current_packages[pkg] = eval(f'{pkg}.__version__')
-    diffs = 0
-    for pkg in frozen_packages.keys():
-        if frozen_packages[pkg] != current_packages[pkg]:
-            diffs += 1
-            msg = (
-                f"The last meta package for PySAL included {pkg} "
-                f"at version {frozen_packages[pkg]}, but you have "
-                f"version {current_packages[pkg]}."
-                )
-            warnings.warn(msg)
-    if diffs == 0:
-        print(f'PySAL subpackages all pinned to meta release {__version__}.')
--- pysal/base.py
+++ pysal/base.py
@@ -28,3 +28,93 @@ memberships = {'libpysal': 'lib',
                'splot': 'viz',
                'mapclassify': 'viz'
 }
+
+
+class cached_property(object):
+    """ A property that is only computed once per instance and then replaces
+        itself with an ordinary attribute. Deleting the attribute resets the
+        property.
+
+        Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
+        """
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
+
+def _installed_version(package):
+    exec(f'import {package}')
+    try:
+        v = eval(f'{package}.__version__')  ## FIX when spvcm is 0.3.0
+    except:
+        v = 'NA'
+    return v
+
+def _installed_versions():
+    ver = {}
+    for package in memberships.keys():
+        ver[package] = _installed_version(package)
+    return ver
+
+def _released_versions():
+    from .frozen import frozen_packages
+    return frozen_packages
+
+
+class Versions:
+    @cached_property
+    def installed(self):
+        """
+        Inventory versions of pysal packages that are installed
+
+        Attributes
+        ----------
+        installed: dict
+                   key is package name, value is version string
+        """
+        return _installed_versions()
+
+    @cached_property
+    def released(self):
+        """
+        Inventory versions of pysal packages that are released in the meta
+        package.
+
+        Attributes
+        ----------
+        released: dict
+                   key is package name, value is version string
+        """
+
+        return _released_versions()
+
+    def check(self):
+        """
+        Print a tabular string that reports installed and released versions of
+        PySAL packages.
+        """
+        table = []
+        package = "Package"
+        installed = "Installed"
+        released = "Released"
+        match = "Match"
+        s = f'{package:>12} | {installed:>10} | {released:>13} | {match:>5}'
+        table.append(s)
+        table.append("-"*len(s))
+        for package in self.installed:
+            installed = self.installed[package]
+            released = self.released[package]
+            match = installed == released
+            s = f'{package:>12} | {installed:>10} | {released:>13} | {match:>5}'
+            table.append(s)
+        print("\n".join(table))
+
+
+versions = Versions()
+
