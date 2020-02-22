e0ba240ddbfc3594d518c2ee21e05e8912912146
Serge Rey <sjsrey@gmail.com>
2020-02-22 09:14:36 -0800

    VER: bump version for next release

--- pysal/__init__.py
+++ pysal/__init__.py
@@ -1,8 +1,7 @@
-__version__='2.2.0'
+__version__ = '2.2.1'
 from . import lib
 from . import explore
 from . import viz
 from . import model
 
 from .base import memberships, federation_hierarchy, versions
-
