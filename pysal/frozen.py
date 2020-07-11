92719cd258ba50d6b86a62f1eef3d3eec7fcffc4
Serge Rey <sjsrey@gmail.com>
2020-07-11 15:15:02 -0700

    [ENH] updating package versions

--- pysal/frozen.py
+++ pysal/frozen.py
@@ -2,11 +2,20 @@
 Frozen subpackages for meta release.
 """
 
-frozen_packages = {"libpysal": "4.2.2", "esda": "2.2.1",
-                    "giddy": "2.3.0", "inequality": "1.0.0",
-                    "pointpats": "2.1.0", "segregation": "1.2.0",
-                    "spaghetti": "1.4.2.post2", "mgwr": "2.1.1",
-                    "spglm": "1.0.7", "spint": "1.0.6",
-                    "spreg": "1.1.1", "spvcm": "0.3.0",
-                    "tobler": "0.2.1", "mapclassify": "2.2.0",
-                    "splot": "1.1.2", "access": "1.0.1"}
+frozen_packages = {    "libpysal": "4.3.0",
+    "access": "1.1.1",
+    "esda": "2.3.0",
+    "giddy": "2.3.3",
+    "inequality": "1.0.0",
+    "pointpats": "2.1.0",
+    "segregation": "1.3.0",
+    "spaghetti": "1.5.0",
+    "mgwr": "2.1.1",
+    "spglm": "1.0.7",
+    "spint": "1.0.6",
+    "spreg": "1.1.1",
+    "spvcm": "0.3.0",
+    "tobler": "0.3.1",
+    "mapclassify": "2.3.0",
+    "splot": "1.1.3"
+}
--- tools/build.py
+++ tools/build.py
@@ -7,6 +7,7 @@ from yolk.pypi import CheeseShop
 
 packages = """
 libpysal
+access
 esda
 giddy
 inequality
